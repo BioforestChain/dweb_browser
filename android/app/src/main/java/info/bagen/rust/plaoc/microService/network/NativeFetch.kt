cc2c64d7b45c9d8c9bc9ef0ea24d9ffd757ae3cf
kezhaofeng <GaubeeBangeel@Gmail.com>
2023-02-18 14:40:58 +0800

    :sparkles: 增加了localeFileFetch的定义

--- android/app/src/main/java/info/bagen/rust/plaoc/microService/network/NativeFetch.kt
+++ android/app/src/main/java/info/bagen/rust/plaoc/microService/network/NativeFetch.kt
@@ -10,10 +10,19 @@ import org.http4k.core.Uri
 
 var fetchAdaptor: (suspend (remote: MicroModule, request: Request) -> Response?)? = null
 
+suspend fun localeFileFetch(remote: MicroModule, request: Request): Response? {
+    if (request.uri.scheme == "file:" && request.uri.host == "/") {
+        /// TODO 从本地文件读取
+    }
+    return null
+}
+
 val client = OkHttp()
 
 suspend fun NativeMicroModule.nativeFetch(request: Request): Response {
-    return fetchAdaptor?.let { it(this, request) } ?: client(request)
+    return fetchAdaptor?.let { it(this, request) }
+        ?: localeFileFetch(this, request)
+        ?: client(request)
 }
 
 suspend fun NativeMicroModule.nativeFetch(url: Uri) = nativeFetch(Request(Method.GET, url))
