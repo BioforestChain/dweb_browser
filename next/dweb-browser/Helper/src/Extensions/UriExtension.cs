a6aeeb72a0b5648e1aeb8aa1392cd01218873522
Kingsword <kingsword09@gmail.com>
2023-04-21 08:55:35 +0800

    :recycle: [next C#]优化UriExtensions Path功能

--- next/dweb-browser/Helper/src/Extensions/UriExtension.cs
+++ next/dweb-browser/Helper/src/Extensions/UriExtension.cs
@@ -45,36 +45,8 @@ public static class UriExtension
     public static Uri SetAuthority(this Uri self, string authority) =>
         new(self.AbsoluteUri.Replace(self.Authority, authority));
 
-    //public static Uri Path(this Uri self, string path) =>
-    //    new(self.AbsoluteUri.Replace(self.AbsolutePath, path.Replace("%2f", "/")));
-
-    public static Uri Path(this Uri self, string path)
-    {
-        //Console.WriteLine($"self: {self}");
-        //Console.WriteLine($"AbsoluteUri: {self.AbsoluteUri}");
-        //Console.WriteLine($"AbsolutePath: {self.AbsolutePath}");
-        //Console.WriteLine($"path: {path}");
-        try
-        {
-            Uri url;
-            if (self.AbsolutePath is "/")
-            {
-                url = new Uri($"{self.AbsoluteUri.Substring(0, self.AbsoluteUri.Length - 1)}{path.Replace("%2f", "/")}");
-            }
-            else
-            {
-                url = new Uri(self.AbsoluteUri.Replace(self.AbsolutePath, path.Replace("%2f", "/")));
-            }
-            return url;
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine($"uri path {e.StackTrace}");
-            Console.WriteLine($"uri path {e.Message}");
-            //throw e;
-            return self;
-        }
-    }
+    public static Uri Path(this Uri self, string path) =>
+        new($"{self.AbsoluteUri.Substring(0, self.AbsoluteUri.Length - self.AbsolutePath.Length)}{path.Replace("%2f", "/")}");
 
 }
 
