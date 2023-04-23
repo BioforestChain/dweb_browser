eb36a66449591cba34b2449a20eed8175528c3e1
waterbang <38105714+waterbang@users.noreply.github.com>
2023-04-23 17:27:34 +0800

    🐛 [android] fix a bug for barcode-scanning

--- plugins/src/components/barcode-scanning/barcode-scanning.wc.ts
+++ plugins/src/components/barcode-scanning/barcode-scanning.wc.ts
@@ -13,16 +13,25 @@ export class HTMLDwebBarcodeScanningElement extends HTMLElement {
   private _canvas: HTMLCanvasElement | null = null;
   private _formats = SupportedFormat.QR_CODE;
   private _direction: string = CameraDirection.BACK;
-  private _activity = false
+  private _activity = false;
 
   constructor() {
     super();
+    this.createClose();
+  }
+
+  private createClose() {
     const closer = new CloseWatcher();
     closer.addEventListener("close", (event) => {
-      console.log("CloseWatcher stopScanning", event.isTrusted, event.timeStamp);
+      console.log(
+        "CloseWatcher stopScanning",
+        event.isTrusted,
+        event.timeStamp
+      );
       if (this._activity) {
-        this.stopScanning()
+        this.stopScanning();
       }
+      this.createClose();
     });
   }
 
@@ -33,26 +42,26 @@ export class HTMLDwebBarcodeScanningElement extends HTMLElement {
    */
   @cacheGetter()
   get getView() {
-    return this._video?.parentElement
+    return this._video?.parentElement;
   }
 
   @cacheGetter()
   get process() {
-    return this.plugin.process
+    return this.plugin.process;
   }
   @cacheGetter()
   get stop() {
-    return this.plugin.stop
+    return this.plugin.stop;
   }
 
   @cacheGetter()
   get toggleTorch() {
-    return torchPlugin.toggleTorch
+    return torchPlugin.toggleTorch;
   }
 
   @cacheGetter()
   get getTorchState() {
-    return torchPlugin.getTorchState
+    return torchPlugin.getTorchState;
   }
 
   // @cacheGetter()
@@ -72,10 +81,10 @@ export class HTMLDwebBarcodeScanningElement extends HTMLElement {
   };
   /**
    * 启动扫码
-   * @returns 
+   * @returns
    */
   async startScanning(rotation = 0, formats = SupportedFormat.QR_CODE) {
-    this.createElement();
+    await this.createElement();
     await this._startVideo();
     return await this.taskPhoto(rotation, formats);
   }
@@ -83,10 +92,9 @@ export class HTMLDwebBarcodeScanningElement extends HTMLElement {
    * 停止扫码
    */
   stopScanning() {
-    this._activity = false
-    this.stopCamera("user stop")
+    this._activity = false;
+    this.stopCamera("user stop");
   }
-
 
   // deno-lint-ignore no-explicit-any
   private stopCamera(error: any) {
@@ -96,14 +104,17 @@ export class HTMLDwebBarcodeScanningElement extends HTMLElement {
 
   /**
    * 不断识图的任务
-   * @returns 
+   * @returns
    */
-  private taskPhoto(rotation: number, formats: SupportedFormat): Promise<string[]> {
-    this._activity = true
+  private taskPhoto(
+    rotation: number,
+    formats: SupportedFormat
+  ): Promise<string[]> {
+    this._activity = true;
     return new Promise((resolve, reject) => {
       const task = () => {
-        if (!this._canvas) return reject("service close！")
-        if (!this._activity) return reject("user close")
+        if (!this._canvas) return reject("service close！");
+        if (!this._activity) return reject("user close");
         this._canvas.toBlob(
           async (imageBlob) => {
             if (imageBlob) {
@@ -111,13 +122,13 @@ export class HTMLDwebBarcodeScanningElement extends HTMLElement {
                 .process(imageBlob, rotation, formats)
                 .then((res) => res)
                 .catch(() => {
-                  this._activity = false
+                  this._activity = false;
                   return reject("502 service error");
                 });
               const result = Array.from(value ?? []);
               if (result.length > 0) {
                 this.stopCamera(result);
-                this._activity = false
+                this._activity = false;
                 return resolve(result);
               }
               return task();
@@ -126,9 +137,9 @@ export class HTMLDwebBarcodeScanningElement extends HTMLElement {
           "image/jpeg",
           0.5 // lossy compression
         );
-      }
-      task()
-    })
+      };
+      task();
+    });
   }
 
   /**
@@ -141,11 +152,10 @@ export class HTMLDwebBarcodeScanningElement extends HTMLElement {
       const constraints: MediaStreamConstraints = {
         video: { facingMode: this._direction },
       };
-
       await navigator.mediaDevices
         .getUserMedia(constraints)
-        .then((stream) => {
-          this.gotMedia(stream);
+        .then(async (stream) => {
+          await this.gotMedia(stream);
         })
         .catch((e) => {
           console.error("getUserMedia() failed: ", e);
@@ -162,33 +172,43 @@ export class HTMLDwebBarcodeScanningElement extends HTMLElement {
    * 拿到帧对象
    * @param mediastream
    */
-  private gotMedia(mediastream: MediaStream) {
+  private async gotMedia(mediastream: MediaStream) {
     if (!this._video) {
       throw new Error("not create video");
     }
     this._video.srcObject = mediastream;
     const videoTracks = mediastream.getVideoTracks();
     if (videoTracks.length > 0 && this._canvas) {
-      this._canvas.captureStream(100)
-      this._video.parentElement?.setAttribute("style", `
-      position:fixed; top: 0; left: 0; width:100%; height: 100%; background-color: black;
-      -webkit-transition:all 0.5s linear;
-      -moz-transition:all 0.5s linear;
-      -ms-transition:all 0.5s linear;
-      -o-transition:all 0.5s linear;
-      transition:all 0.5s linear;
-      visibility: visible;`);
+      this._canvas.captureStream(100);
       const ctx = this._canvas.getContext("2d");
       // 压缩为 100 * 100
-      const update = () => requestAnimationFrame(() => {
-        if (ctx && this._video) {
-          ctx.drawImage(this._video, 0, 0, this._canvas?.width ?? 480, this._canvas?.height ?? 360);
-          update()
-        }
-      });
-      update()
+      const update = () =>
+        requestAnimationFrame(() => {
+          if (ctx && this._video) {
+            ctx.drawImage(
+              this._video,
+              0,
+              0,
+              this._canvas?.width ?? 480,
+              this._canvas?.height ?? 360
+            );
+            update();
+          }
+        });
+      update();
     }
-    this._video.play();
+    await this._video.play();
+    this._video?.parentElement?.setAttribute(
+      "style",
+      `
+      position:fixed; top: 0; left: 0; width:100%; height: 100%; background-color: black;
+      -webkit-transition:all 0.2s linear;
+      -moz-transition:all 0.2s linear;
+      -ms-transition:all 0.2s linear;
+      -o-transition:all 0.2s linear;
+      transition:all 0.2s linear;
+      visibility: visible;`
+    );
   }
 
   private _stop() {
@@ -207,8 +227,10 @@ export class HTMLDwebBarcodeScanningElement extends HTMLElement {
       this._video = null;
     }
     if (this._canvas) {
-      this._canvas.getContext("2d")?.clearRect(0, 0, this._canvas.width, this._canvas.height)
-      this._canvas = null
+      this._canvas
+        .getContext("2d")
+        ?.clearRect(0, 0, this._canvas.width, this._canvas.height);
+      this._canvas = null;
     }
   }
 
@@ -218,54 +240,59 @@ export class HTMLDwebBarcodeScanningElement extends HTMLElement {
    * @returns
    */
   private createElement(direction: CameraDirection = CameraDirection.BACK) {
-    const body = document.body;
+    return new Promise((resolve, reject) => {
+      const body = document.body;
 
-    const video = document.getElementById("video");
-    const canvas = document.getElementById("canvas");
+      const video = document.getElementById("video");
+      const canvas = document.getElementById("canvas");
 
-    if (video) {
-      return { message: "camera already started" };
-    }
-    const parent = document.createElement("div");
-    parent.setAttribute(
-      "style",
-      "position:fixed; top: 0; left: 0; width:100%; height: 100%; background-color: black;visibility: hidden;"
-    );
-    this._video = document.createElement("video");
-    this._video.id = "video";
-    // 如果摄像头朝后，请勿翻转视频源
-    if (direction !== CameraDirection.BACK) {
-      this._video.setAttribute(
+      if (video) {
+        reject("camera already started");
+        return { message: "camera already started" };
+      }
+      const parent = document.createElement("div");
+      parent.setAttribute(
         "style",
-        "-webkit-transform: scaleX(-1); transform: scaleX(-1); width:100%; height: 100%;"
+        "position:fixed; top: 0; left: 0; width:100%; height: 100%; background-color: black;visibility: hidden;"
       );
-    } else {
-      this._video.setAttribute("style", "width:100%; height: 100%;");
-    }
-    this._video.setAttribute("autoplay", "true");
+      this._video = document.createElement("video");
+      this._video.id = "video";
 
-    const userAgent = navigator.userAgent.toLowerCase();
-    const isSafari =
-      userAgent.includes("safari") && !userAgent.includes("chrome");
+      // 如果摄像头朝后，请勿翻转视频源
+      if (direction !== CameraDirection.BACK) {
+        this._video.setAttribute(
+          "style",
+          "-webkit-transform: scaleX(-1); transform: scaleX(-1); width:100%; height: 100%;"
+        );
+      } else {
+        this._video.setAttribute("style", "width:100%; height: 100%;");
+      }
+      this._video.setAttribute("autoplay", "true");
 
-    // iOS 上的 Safari 需要设置 autoplay、muted 和 playsinline 属性，video.play() 才能成功
-    // 如果没有这些属性，this.video.play() 将抛出 NotAllowedError
-    // https://developer.apple.com/documentation/webkit/delivering_video_content_for_safari
-    if (isSafari) {
-      this._video.setAttribute("muted", "true");
-      this._video.setAttribute("playsinline", "true");
-    }
+      const userAgent = navigator.userAgent.toLowerCase();
+      const isSafari =
+        userAgent.includes("safari") && !userAgent.includes("chrome");
 
-    parent.appendChild(this._video);
-    if (!canvas) {
-      this._canvas = document.createElement("canvas");
-      this._canvas.setAttribute("style", "visibility: hidden;");
-      this._canvas.width = 480
-      this._canvas.height = 360
-      this._canvas.id = "canvas";
-      parent.appendChild(this._canvas);
-    }
-    body.appendChild(parent);
+      // iOS 上的 Safari 需要设置 autoplay、muted 和 playsinline 属性，video.play() 才能成功
+      // 如果没有这些属性，this.video.play() 将抛出 NotAllowedError
+      // https://developer.apple.com/documentation/webkit/delivering_video_content_for_safari
+      if (isSafari) {
+        this._video.setAttribute("muted", "true");
+        this._video.setAttribute("playsinline", "true");
+      }
+
+      parent.appendChild(this._video);
+      if (!canvas) {
+        this._canvas = document.createElement("canvas");
+        this._canvas.setAttribute("style", "visibility: hidden;");
+        this._canvas.width = 480;
+        this._canvas.height = 360;
+        this._canvas.id = "canvas";
+        parent.appendChild(this._canvas);
+      }
+      body.appendChild(parent);
+      resolve(true);
+    });
   }
 }
 
