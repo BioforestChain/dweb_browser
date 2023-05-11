4ce446791b1e18b14e063db911cd2d2d2acd8963
kingsword09 <kingsword09@gmail.com>
2023-05-11 14:52:31 +0800

    :sparkles: [next C#] 添加Haptics

---
+++ next/dweb-browser/src/Platforms/iOS/MicroModule/Plugin/Haptics/HapticsNMM.cs
@@ -0,0 +1,127 @@
+﻿using UIKit;
+using System.Net;
+using System.Text.Json;
+using DwebBrowser.MicroService.Http;
+
+namespace DwebBrowser.Platforms.iOS.MicroModule.Plugin.Haptics;
+
+public class HapticsNMM : NativeMicroModule
+{
+    public HapticsNMM() : base("haptics.sys.dweb")
+    {
+    }
+
+    protected override async Task _bootstrapAsync(IBootstrapContext bootstrapContext)
+    {
+        /// 触碰轻质量物体
+        HttpRouter.AddRoute(IpcMethod.Get, "/impactLight", async (request, _) =>
+        {
+            var style = request.QueryString("style") switch
+            {
+                string s when s.ToUpper() == "MEDIUM" => UIImpactFeedbackStyle.Medium,
+                string s when s.ToUpper() == "HEAVY" => UIImpactFeedbackStyle.Heavy,
+                _ => UIImpactFeedbackStyle.Light
+            };
+
+            // 初始化反馈
+            var impact = new UIImpactFeedbackGenerator(style);
+            // 通知系统即将发生触觉反馈
+            impact.Prepare();
+
+            // 触发反馈
+            impact.ImpactOccurred();
+            return new PureResponse(HttpStatusCode.OK);
+        });
+
+        /// 警告分隔的振动通知
+        HttpRouter.AddRoute(IpcMethod.Get, "/notification", async (request, _) =>
+        {
+            var type = request.QueryStringRequired("duration") switch
+            {
+                string t when t.ToUpper() == "SUCCESS" => UINotificationFeedbackType.Success,
+                string t when t.ToUpper() == "WARNING" => UINotificationFeedbackType.Warning,
+                _ => UINotificationFeedbackType.Error,
+            };
+
+            // 初始化反馈
+            var notification = new UINotificationFeedbackGenerator();
+            // 通知系统即将发生触觉反馈
+            notification.Prepare();
+
+            // 触发反馈
+            notification.NotificationOccurred(type);
+            return new PureResponse(HttpStatusCode.OK);
+        });
+
+        /// 单击手势的反馈振动
+        HttpRouter.AddRoute(IpcMethod.Get, "/vibrateClick", async (request, _) =>
+        {
+            if (await VibrateManager.VibrateAsync(VibrateManager.VibrateType.Click))
+            {
+                return new PureResponse(HttpStatusCode.OK);
+            }
+
+            return new PureResponse(HttpStatusCode.InternalServerError);
+        });
+
+        /// 禁用手势的反馈振动，与headShak特效一致
+        HttpRouter.AddRoute(IpcMethod.Get, "/vibrateDisabled", async (request, _) =>
+        {
+            if (await VibrateManager.VibrateAsync(VibrateManager.VibrateType.Disabled))
+            {
+                return new PureResponse(HttpStatusCode.OK);
+            }
+
+            return new PureResponse(HttpStatusCode.InternalServerError);
+        });
+
+        /// 双击手势的反馈振动
+        HttpRouter.AddRoute(IpcMethod.Get, "/vibrateDoubleClick", async (request, _) =>
+        {
+            if (await VibrateManager.VibrateAsync(VibrateManager.VibrateType.DoubleClick))
+            {
+                return new PureResponse(HttpStatusCode.OK);
+            }
+
+            return new PureResponse(HttpStatusCode.InternalServerError);
+        });
+
+        /// 重击手势的反馈振动，比如菜单键/长按/3DTouch
+        HttpRouter.AddRoute(IpcMethod.Get, "/vibrateHeavyClick", async (request, _) =>
+        {
+            if (await VibrateManager.VibrateAsync(VibrateManager.VibrateType.HeavyClick))
+            {
+                return new PureResponse(HttpStatusCode.OK);
+            }
+
+            return new PureResponse(HttpStatusCode.InternalServerError);
+        });
+
+        /// 滴答
+        HttpRouter.AddRoute(IpcMethod.Get, "/vibrateTick", async (request, _) =>
+        {
+            if (await VibrateManager.VibrateAsync(VibrateManager.VibrateType.Tick))
+            {
+                return new PureResponse(HttpStatusCode.OK);
+            }
+
+            return new PureResponse(HttpStatusCode.InternalServerError);
+        });
+
+        /// 自定义传递 振动频率
+        HttpRouter.AddRoute(IpcMethod.Get, "/customize", async (request, _) =>
+        {
+            var duration = request.QueryStringRequired("duration");
+            var durationType = typeof(List<double>)!;
+            var durationList = (List<double>)JsonSerializer.Deserialize(duration, durationType)!;
+
+            if (await VibrateManager.VibrateAsync(VibrateManager.VibrateType.Customize, durationList.ToArray()))
+            {
+                return new PureResponse(HttpStatusCode.OK);
+            }
+
+            return new PureResponse(HttpStatusCode.InternalServerError);
+        });
+    }
+}
+
---
+++ next/dweb-browser/src/Platforms/iOS/MicroModule/Plugin/Haptics/VibrateManager.cs
@@ -0,0 +1,142 @@
+﻿using CoreHaptics;
+using AudioToolbox;
+
+#nullable enable
+
+namespace DwebBrowser.Platforms.iOS.MicroModule.Plugin.Haptics;
+
+public static class VibrateManager
+{
+    static Debugger Console = new("VibrateManager");
+    static bool s_isSupportHaptics = CHHapticEngine.GetHardwareCapabilities().SupportsHaptics;
+
+    public enum VibrateType
+    {
+        Click,
+        Disabled,
+        DoubleClick,
+        HeavyClick,
+        Tick,
+        Customize
+    }
+
+    public static async Task<bool> VibrateAsync(VibrateType type, double[]? durationArr = null)
+    {
+        if (!s_isSupportHaptics)
+        {
+            Console.Log("SupportsHaptics", "No support haptics");
+            return false;
+        }
+
+        switch (type)
+        {
+            case VibrateType.Click:
+                durationArr = new double[] { 1 };
+                break;
+            case VibrateType.Disabled:
+                durationArr = new double[] { 1, 63, 1, 119, 1, 129, 1 };
+                break;
+            case VibrateType.DoubleClick:
+                durationArr = new double[] { 10, 1 };
+                break;
+            case VibrateType.HeavyClick:
+                durationArr = new double[] { 1, 100, 1, 1 };
+                break;
+            case VibrateType.Tick:
+                durationArr = new double[] { 10, 999, 1, 1 };
+                break;
+            case VibrateType.Customize:
+                if (durationArr is null)
+                {
+                    Console.Log("Vibrate", "custom vibrate duration is null");
+                    return false;
+                }
+                break;
+        }
+
+        try
+        {
+            // 创建引擎
+            var engine = new CHHapticEngine(out var error);
+
+            if (error is not null)
+            {
+                Console.Log("Vibrate", "error: {0}", error.LocalizedDescription);
+                await SystemSound.Vibrate.PlayAlertSoundAsync();
+                return false;
+            }
+
+            await engine.StartAsync();
+
+            engine.ResetHandler = async () =>
+            {
+                try
+                {
+                    await engine.StartAsync();
+                }
+                catch
+                {
+                    await SystemSound.Vibrate.PlayAlertSoundAsync();
+                }
+            };
+
+
+            var eventsList = new List<CHHapticEvent>();
+            var i = 0;
+            var relativeTime = 0.0;
+            foreach (var duration in durationArr!)
+            {
+                if (i % 2 == 0)
+                {
+                    var intensity = new CHHapticEventParameter(CHHapticEventParameterId.HapticIntensity, (float)0.5);
+                    var sharpness = new CHHapticEventParameter(CHHapticEventParameterId.HapticSharpness, (float)0.6);
+                    var continuousEvent = new CHHapticEvent(
+                        CHHapticEventType.HapticContinuous,
+                        new[] { intensity, sharpness },
+                        relativeTime,
+                        Math.Max(0.01, duration / 1000));
+                    eventsList.Append(continuousEvent);
+                }
+
+                Console.Log("Vibrate", "relativeTime: {0}, duration: {1}", relativeTime, duration / 1000);
+                relativeTime += duration / 1000;
+            }
+
+            var pattern = new CHHapticPattern(eventsList.ToArray(), new CHHapticDynamicParameter[0], out var outError);
+
+            if (outError is not null)
+            {
+                Console.Log("Vibrate", "outError: {0}", outError.LocalizedDescription);
+                await SystemSound.Vibrate.PlayAlertSoundAsync();
+                return false;
+            }
+
+            var player = engine.CreateAdvancedPlayer(pattern, out var createError);
+
+            if (createError is not null)
+            {
+                Console.Log("Vibrate", "createError: {0}", createError.LocalizedDescription);
+                await SystemSound.Vibrate.PlayAlertSoundAsync();
+                return false;
+            }
+
+            player.Start(0, out var startError);
+
+            if (startError is not null)
+            {
+                Console.Log("Vibrate", "startError: {0}", startError.LocalizedDescription);
+                await SystemSound.Vibrate.PlayAlertSoundAsync();
+                return false;
+            }
+        }
+        catch (Exception e)
+        {
+            Console.Log("Vibrate", "exception: {0}", e.Message);
+            await SystemSound.Vibrate.PlayAlertSoundAsync();
+            return false;
+        }
+
+        return true;
+    }
+}
+
