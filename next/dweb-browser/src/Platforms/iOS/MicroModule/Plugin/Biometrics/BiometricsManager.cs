93af4c69bc459971be8e7cb0327e7c1d5328f477
kingsword09 <kingsword09@gmail.com>
2023-05-24 10:06:03 +0800

    :sparkles: [next C#] 添加生物识别模块

---
+++ next/dweb-browser/src/Platforms/iOS/MicroModule/Plugin/Biometrics/BiometricsManager.cs
@@ -0,0 +1,34 @@
+﻿using LocalAuthentication;
+
+namespace DwebBrowser.Platforms.iOS.MicroModule.Plugin.Biometrics;
+
+public static class BiometricsManager
+{
+	static Debugger Console = new("BiometricsManager");
+
+	private static LAContext _context = new LAContext();
+
+	public static bool Check()
+	{
+		var _bool = _context.CanEvaluatePolicy(LAPolicy.DeviceOwnerAuthenticationWithBiometrics, out var error);
+		
+		if (error is not null)
+		{
+			Console.Log("check", error.LocalizedDescription);
+		}
+
+		return _bool;
+	}
+
+	public static async Task<BiometricsResult> BiometricsAsync()
+	{
+		var (_bool, error) = await _context.EvaluatePolicyAsync(
+			LAPolicy.DeviceOwnerAuthenticationWithBiometrics,
+			"Access requires authentication");
+
+		return new BiometricsResult(_bool, error?.LocalizedDescription ?? "");
+    }
+
+	public record BiometricsResult(bool success, string message);
+}
+
