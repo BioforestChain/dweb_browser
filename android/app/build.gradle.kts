cdbe9e34cad389ecab3926e4774f303c2fff2fa3
jackie-yellow <hl19081555@gmail.com>
2023-05-31 18:35:59 +0800

    :bookmark:                     发布1.1版本

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,8 +9,8 @@ android {
         applicationId = "info.bagen.dwebbrowser"
         minSdk = 28
         targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
