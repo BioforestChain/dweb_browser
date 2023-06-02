be39fccbf706d11025ee89c01d8116cb8e435448
pxh <575527452@qq.com>
2023-06-02 09:40:56 +0800

    🚧 [desktop-dev] browser 入口

--- desktop-dev/src/browser/browser/browser.ts
+++ desktop-dev/src/browser/browser/browser.ts
@@ -1,22 +1,35 @@
 import path from "node:path";
 import { $BootstrapContext } from "../../core/bootstrapContext.ts";
 import { NativeMicroModule } from "../../core/micro-module.native.ts";
+import { HttpDwebServer } from "../../sys/http-server/$createHttpDwebServer.ts";
+import { createWWWServer } from "./broser.www.serve.ts"
+
 
 export class BrowserNMM extends NativeMicroModule {
   mmid = "browser.dweb" as const;
+  wwwServer: HttpDwebServer | undefined;
   protected async _bootstrap(context: $BootstrapContext) {
-    await Electron.app.whenReady();
-    const window = new Electron.BrowserWindow({
-      webPreferences: {
-        devTools: true,
-      },
-    });
-    const index_html = path.resolve(
-      Electron.app.getAppPath(),
-      "assets/browser/newtab/index.html"
-    );
-    console.log("index_html", index_html);
-    window.loadFile(index_html);
+    {
+      await createWWWServer.bind(this)()
+    }
+    const URLInfo = this.wwwServer?.startResult.urlInfo.buildInternalUrl("/")!
+    this.nativeFetch(
+      `file://mwebview.browser.dweb/open?url=${URLInfo.href}`
+    )
+
+    // await Electron.app.whenReady();
+    // const window = new Electron.BrowserWindow({
+    //   webPreferences: {
+    //     devTools: true,
+    //   },
+    // });
+    // const index_html = path.resolve(
+    //   Electron.app.getAppPath(),
+    //   "assets/browser/newtab/index.html"
+    // );
+    // console.log("index_html", index_html);
+    // // 只能够载入 mubltiwebviw
+    // window.loadFile(index_html);
   }
   protected _shutdown() {}
 }
