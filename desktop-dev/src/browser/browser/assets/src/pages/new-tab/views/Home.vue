514d11a26ac44944d49c67f4c601f1c83efb2206
pxh <575527452@qq.com>
2023-06-03 21:17:33 +0800

    📝 [desktop-dev] 更新browser UI 的类型文件

--- desktop-dev/src/browser/browser/assets/src/pages/new-tab/views/Home.vue
+++ desktop-dev/src/browser/browser/assets/src/pages/new-tab/views/Home.vue
@@ -1,46 +1,7 @@
 <script lang="ts" setup>
+import type { $AppMetaData } from "@/types/app.type"
 import jmm from "../components/JMM.vue";
 import { onBeforeMount, reactive } from 'vue'
-
-export interface $AppMetaData {
-  title: string;
-  subtitle: string;
-  id: string;
-  bundleUrl: string;
-  bundleHash: string;
-  bundleSize: number;
-  icon: string;
-  images: string[];
-  introduction: string;
-  author: string[];
-  version: string;
-  keywords: string[];
-  home: string;
-  mainUrl: string;
-  server: {
-    root: string;
-    entry: string;
-  };
-  splashScreen: { entry: string };
-  staticWebServers: $StaticWebServers[];
-  openWebViewList: [];
-  permissions: string[];
-  plugins: string[];
-  releaseDate: string;
-}
-
-export interface $StaticWebServers {
-  root: string;
-  entry: string;
-  subdomain: string;
-  port: number;
-}
-
-// type $DWEB_DEEPLINK = `dweb:${string}`;
-interface $MainServer {
-    root: string;
-    entry: string;
-  }
 
 const state: {
   appsInfo: $AppMetaData[]
--- desktop-dev/src/browser/browser/assets/src/types/app.type.ts
+++ desktop-dev/src/browser/browser/assets/src/types/app.type.ts
@@ -1,5 +1,33 @@
 export interface $AppMetaData {
   title: string;
+  subtitle: string;
   id: string;
+  bundleUrl: string;
+  bundleHash: string;
+  bundleSize: number;
   icon: string;
+  images: string[];
+  introduction: string;
+  author: string[];
+  version: string;
+  keywords: string[];
+  home: string;
+  mainUrl: string;
+  server: {
+    root: string;
+    entry: string;
+  };
+  splashScreen: { entry: string };
+  staticWebServers: $StaticWebServers[];
+  openWebViewList: [];
+  permissions: string[];
+  plugins: string[];
+  releaseDate: string;
+}
+
+export interface $StaticWebServers {
+  root: string;
+  entry: string;
+  subdomain: string;
+  port: number;
 }
