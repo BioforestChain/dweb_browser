cadc14015c9a30e816bade7993509f0eaa7fa65c
waterbang <409071692@qq.com>
2023-06-14 15:23:46 +0800

    :recycle: [android] refactor read file

--- android/app/src/main/java/info/bagen/dwebbrowser/microService/sys/dns/NativeFetch.kt
+++ android/app/src/main/java/info/bagen/dwebbrowser/microService/sys/dns/NativeFetch.kt
@@ -38,272 +38,194 @@ private val mimeTypeMap by lazy { MimeTypeMap.getSingleton() }
 /**
  * 安全的 AssetManager 文件读取工具
  */
-class ChunkAssetsFileStream(
-    val src: String,
-    val chunkSize: Int = defaultChunkSize,
-    override val preReadableSize: Int = chunkSize
+private class ChunkAssetsFileStream(
+  val src: String,
+  val chunkSize: Int = defaultChunkSize,
+  override val preReadableSize: Int = chunkSize,
+  isSys: Boolean = true
 ) : InputStream(), PreReadableInputStream {
-    companion object {
-        /// 默认1mb切一次
-        val defaultChunkSize = 1024 * 1024
-        private var acc_id = AtomicInteger(1)
+  companion object {
+    /// 默认1mb切一次
+    val defaultChunkSize = 1024 * 1024
+    private var acc_id = AtomicInteger(1)
+  }
+
+  val id = acc_id.getAndAdd(1)
+
+  init {
+    debugFetchFile("INIT", "$id/$src")
+  }
+
+  val source by lazy {
+    if (isSys) {
+      return@lazy App.appContext.assets.open(
+        src, AssetManager.ACCESS_STREAMING
+      )
     }
+    return@lazy File(src).inputStream()
+  }
 
-    val id = acc_id.getAndAdd(1)
+  var ptr = 0
+  val totalSize by lazy { source.available() }
 
-    init {
-        debugFetchFile("INIT", "$id/$src")
-    }
+  override fun read() = source.read()
+  override fun available() = chunkSize.coerceAtMost(totalSize - ptr)
 
-    val source by lazy {
-        App.appContext.assets.open(
-            src, AssetManager.ACCESS_STREAMING
-        )
-    }
+  var readEnd = false
 
-    var ptr = 0
-    val totalSize by lazy { source.available() }
-
-    override fun read() = source.read()
-    override fun available() = chunkSize.coerceAtMost(totalSize - ptr)
-
-    var readEnd = false
-
-    @Synchronized
-    override fun read(b: ByteArray, off: Int, len: Int) = when (readEnd) {
-        false -> source.read(b, off, len).also { readLen ->
-            if (readLen != len) {
-                // assets 的流内容是明确的，所以一般不会有 available 突变的问题，除非被重写
-                readEnd = true
-                debugFetchFile("READ", "$id/$src($totalSize) $off+$len~>$readLen")
-            }
-            if (readLen == -1) {
-                readEnd = true
-            }
-            ptr += readLen
-        }
-
-        else -> -1
-    }
-
-    override fun close() {
+  @Synchronized
+  override fun read(b: ByteArray, off: Int, len: Int) = when (readEnd) {
+    false -> source.read(b, off, len).also { readLen ->
+      if (readLen != len) {
+        // assets 的流内容是明确的，所以一般不会有 available 突变的问题，除非被重写
         readEnd = true
-        debugFetchFile("CLOSE", "$id/$src")
-        source.close()
-    }
-
-}
-
-/**
- * 安全的 AssetManager 文件读取工具
- */
-class ChunkDataFileStream(
-    val src: String,
-    private val chunkSize: Int = defaultChunkSize,
-    override val preReadableSize: Int = chunkSize
-) : InputStream(), PreReadableInputStream {
-    companion object {
-        /// 默认1mb切一次
-        const val defaultChunkSize = 1024 * 1024
-        private var acc_id = AtomicInteger(1)
-    }
-
-    val id = acc_id.getAndAdd(1)
-
-    init {
-        debugFetchFile("INIT", "$id/$src")
-    }
-
-    val source by lazy { File(src).inputStream() }
-
-    var ptr = 0
-    private val totalSize by lazy { source.available() }
-
-    override fun read() = source.read()
-    override fun available() = chunkSize.coerceAtMost(totalSize - ptr)
-
-    var readEnd = false
-
-    @Synchronized
-    override fun read(b: ByteArray, off: Int, len: Int) = when (readEnd) {
-        false -> source.read(b, off, len).also { readLen ->
-            if (readLen != len) {
-                // assets 的流内容是明确的，所以一般不会有 available 突变的问题，除非被重写
-                readEnd = true
-                debugFetchFile("READ", "$id/$src($totalSize) $off+$len~>$readLen")
-            }
-            if (readLen == -1) {
-                readEnd = true
-            }
-            ptr += readLen
-        }
-
-        else -> -1
-    }
-
-    override fun close() {
+        debugFetchFile("READ", "$id/$src($totalSize) $off+$len~>$readLen")
+      }
+      if (readLen == -1) {
         readEnd = true
-        debugFetchFile("CLOSE", "$id/$src")
-        source.close()
+      }
+      ptr += readLen
     }
+
+    else -> -1
+  }
+
+  override fun close() {
+    readEnd = true
+    debugFetchFile("CLOSE", "$id/$src")
+    source.close()
+  }
+
 }
 
-private fun readUsrFetch(remote: MicroModule, request: Request): Response {
-    runCatching {
-        val mode = request.query("mode") ?: "auto"
-        val chunk = request.query("chunk")?.toIntOrNull() ?: ChunkDataFileStream.defaultChunkSize
-        val preRead = request.query("pre-read")?.toBooleanStrictOrNull() ?: false
+private fun getLocalFetch(remote: MicroModule, request: Request): Response {
+  val path = request.uri.path
+  val isSys = path.startsWith("/sys/")
+  val mode = request.query("mode") ?: "auto"
+  val chunk = request.query("chunk")?.toIntOrNull() ?: ChunkAssetsFileStream.defaultChunkSize
+  val preRead = request.query("pre-read")?.toBooleanStrictOrNull() ?: false
 
-        val src = request.uri.path
-        // 默认是需要 file:///usr/xxx.js
-        val dirname: String = App.appContext.dataDir.absolutePath + File.separator +
-                APP_DIR_TYPE.SystemApp.rootName + File.separator + remote.mmid
-        val filename: String = dirname + src
-        debugFetchFile("OPEN-DataSrc", "dirname=$dirname, src=$src")
+  var src = if (isSys) {
+    request.uri.path.substring(1)
+  } else {
+    request.uri.path
+  }
+  // 如果是sys需要移除sys 然后 转发到assets 如果是usr需要转发到data里
+  if (path.startsWith("/sys/")) {
+    src = src.substring(4)
+  }
 
-        val filenameList = FilesUtil.traverseFileTree(dirname)
+  debugFetchFile("OPEN-Assets $isSys", "src:$src path:$path")
 
-        lateinit var response: Response
-        if (!filenameList.contains(filename)) {
-            debugFetchFile("NO-FOUND-DataSrc", request.uri.path + "-->" + filename)
-            response = Response(Status.NOT_FOUND).body("the file(${request.uri.path}) not found.")
-        } else {
-            response = Response(status = Status.OK)
-
-            // buffer 模式，就是直接全部读取出来
-            // TODO auto 模式就是在通讯次数和单次通讯延迟之间的一个取舍。如果分片次数少于2次，那么就直接发送，没必要分片
-            response = if (mode == "stream") {
-                val streamBody = ChunkDataFileStream(
-                    filename, chunkSize = chunk, preReadableSize = if (preRead) chunk else 0
-                )
-                /**
-                 * 将它分片读取
-                 */
-                response.header("X-Assets-Id", streamBody.id.toString()).body(streamBody)
-            } else {
-                val dataStream = File(filename).inputStream() // 打开一个读取流
-                response.body(MemoryBody(dataStream.readByteArray())) // 一次性发送
-            }
-
-            /// 尝试加入 Content-Type
-            val extension = MimeTypeMap.getFileExtensionFromUrl(request.uri.path)
-            if (extension != null) {
-                val type = mimeTypeMap.getMimeTypeFromExtension(extension)
-                if (type != null) {
-                    response = response.header("Content-Type", type)
-                }
-            }
-            return   response
-        }
-        return response
-    }.getOrElse {
-        return  Response(Status.INTERNAL_SERVER_ERROR)
-    }
-}
-
-fun getLocalFetch(remote: MicroModule,request: Request):Response {
-    val  path = request.uri.path
-    if (path.startsWith("/usr/")) {
-        return readUsrFetch(remote,request)
-    }
-    val mode = request.query("mode") ?: "auto"
-    val chunk = request.query("chunk")?.toIntOrNull() ?: ChunkAssetsFileStream.defaultChunkSize
-    val preRead = request.query("pre-read")?.toBooleanStrictOrNull() ?: false
-
-    var src = request.uri.path.substring(1)
-    // 如果是sys需要移除sys 然后 转发到assets
-    if (path.startsWith("/sys/")) {
-        src = src.substring(4)
-    }
-
-    debugFetchFile("OPEN-Assets", "src:$src path:$path")
-
-    lateinit var dirname: String
-    lateinit var filename: String
-
+  lateinit var dirname: String
+  lateinit var filename: String
+  lateinit var filenameList: Array<String>
+  if (isSys) {
+    // 读取assets的文件
     src.lastIndexOf('/').also { it ->
-        when (it) {
-            -1 -> {
-                filename = src
-                dirname = ""
-            }
-
-            else -> {
-                filename = src.substring(it + 1)
-                dirname = src.substring(0..it)
-            }
+      when (it) {
+        -1 -> {
+          filename = src
+          dirname = ""
         }
-        src.substring(0..it)
+
+        else -> {
+          filename = src.substring(it + 1)
+          dirname = src.substring(0..it)
+        }
+      }
+      src.substring(0..it)
     }
     /// 尝试打开文件，如果打开失败就走 404 no found 响应
-    val filenameList = App.appContext.assets.list(dirname) ?: emptyArray()
+    filenameList = App.appContext.assets.list(dirname) ?: emptyArray()
+  } else {
+    // 读取app内存的文件
+    dirname = App.appContext.dataDir.absolutePath + File.separator +
+        APP_DIR_TYPE.SystemApp.rootName + File.separator + remote.mmid
+    filename = dirname + src
 
-    lateinit var response: Response
-    if (!filenameList.contains(filename)) {
-        debugFetchFile("NO-FOUND-Assets", request.uri.path)
-        response = Response(Status.NOT_FOUND).body("the file(${request.uri.path}) not found.")
+    filenameList = FilesUtil.traverseFileTree(dirname).toTypedArray()
+  }
+  debugFetchFile("OPEN-DataSrc", "dirname=$dirname, src=$src")
+
+  lateinit var response: Response
+  if (!filenameList.contains(filename)) {
+    debugFetchFile("NO-FOUND-Assets", request.uri.path)
+    response = Response(Status.NOT_FOUND).body("the file(${request.uri.path}) not found.")
+  } else {
+    response = Response(status = Status.OK)
+
+    // buffer 模式，就是直接全部读取出来
+    // TODO auto 模式就是在通讯次数和单次通讯延迟之间的一个取舍。如果分片次数少于2次，那么就直接发送，没必要分片
+    response = if (mode == "stream") {
+      val streamBody = if (isSys) {
+        ChunkAssetsFileStream(
+          src, chunkSize = chunk, preReadableSize = if (preRead) chunk else 0, true
+        )
+      } else {
+        ChunkAssetsFileStream(
+          filename, chunkSize = chunk, preReadableSize = if (preRead) chunk else 0, false
+        )
+      }
+      /**
+       * 将它分片读取
+       */
+      response.header("X-Assets-Id", streamBody.id.toString()).body(streamBody)
     } else {
-        response = Response(status = Status.OK)
-
-        // buffer 模式，就是直接全部读取出来
-        // TODO auto 模式就是在通讯次数和单次通讯延迟之间的一个取舍。如果分片次数少于2次，那么就直接发送，没必要分片
-        response = if (mode == "stream") {
-            val streamBody = ChunkAssetsFileStream(
-                src, chunkSize = chunk, preReadableSize = if (preRead) chunk else 0
-            )
-            /**
-             * 将它分片读取
-             */
-            response.header("X-Assets-Id", streamBody.id.toString()).body(streamBody)
-        } else {
-            /**
-             * 打开一个读取流
-             */
-            val assetStream = App.appContext.assets.open(
-                src, AssetManager.ACCESS_BUFFER
-            )
-            /**
-             * 一次性发送
-             */
-            response.body(MemoryBody(assetStream.readByteArray()))
-        }
-
-        /// 尝试加入 Content-Type
-        val extension = MimeTypeMap.getFileExtensionFromUrl(request.uri.path)
-        if (extension != null) {
-            val type = mimeTypeMap.getMimeTypeFromExtension(extension)
-            if (type != null) {
-                response = response.header("Content-Type", type)
-            }
-        }
-      return response
+      /**
+       * 打开一个读取流
+       */
+      val assetStream = if (isSys) {
+        App.appContext.assets.open(
+          src, AssetManager.ACCESS_BUFFER
+        )
+      } else {
+        File(filename).inputStream()
+      }
+      /**
+       * 一次性发送
+       */
+      response.body(MemoryBody(assetStream.readByteArray()))
     }
 
-   return response
+    /// 尝试加入 Content-Type
+    val extension = MimeTypeMap.getFileExtensionFromUrl(request.uri.path)
+    if (extension != null) {
+      val type = mimeTypeMap.getMimeTypeFromExtension(extension)
+      if (type != null) {
+        response = response.header("Content-Type", type)
+      }
+    }
+    return response
+  }
+
+  return response
 }
 
 /**
  * 加载本地文件
  */
 private fun localeFileFetch(remote: MicroModule, request: Request) = when {
-    request.uri.scheme == "file" && request.uri.host == "" -> runCatching {
-      return@runCatching getLocalFetch(remote,request)
-    }.getOrElse {
-        Response(Status.INTERNAL_SERVER_ERROR)
-    }else -> null
+  request.uri.scheme == "file" && request.uri.host == "" -> runCatching {
+    return@runCatching getLocalFetch(remote, request)
+  }.getOrElse {
+    Response(Status.INTERNAL_SERVER_ERROR)
+  }
+
+  else -> null
 }
 
 val networkFetch =
-    ApacheClient(responseBodyMode = BodyMode.Stream, requestBodyMode = BodyMode.Stream)
+  ApacheClient(responseBodyMode = BodyMode.Stream, requestBodyMode = BodyMode.Stream)
 
 suspend fun MicroModule.nativeFetch(request: Request): Response {
-    for (fetchAdapter in nativeFetchAdaptersManager.adapters) {
-        val response = fetchAdapter(this, request)
-        if (response != null) {
-            return response
-        }
+  for (fetchAdapter in nativeFetchAdaptersManager.adapters) {
+    val response = fetchAdapter(this, request)
+    if (response != null) {
+      return response
     }
-    return localeFileFetch(this, request) ?: networkFetch(request)
+  }
+  return localeFileFetch(this, request) ?: networkFetch(request)
 }
 
 suspend inline fun MicroModule.nativeFetch(url: Uri) = nativeFetch(Request(Method.GET, url))
