9c500aa088950eb025a81d253268f948aaa8e04a
kingsword09 <kingsword09@gmail.com>
2023-06-26 17:52:41 +0800

    :bug: [script] 修复windows系统无法找到pnpm执行目录的问题

--- scripts/init.ts
+++ scripts/init.ts
@@ -1,8 +1,14 @@
 import { ConTasks } from "./helper/ConTasks.ts";
+import {
+  which,
+} from "https://deno.land/x/which/mod.ts";
+
+// 为了适配windows平台
+const pathToPnpm = await which("pnpm");
 export const initTasks = new ConTasks(
   {
     pnpm: {
-      cmd: "pnpm",
+      cmd: pathToPnpm!,
       args: "install",
     },
   },
