352b95b9d5cd6012d4ce207a79963d1c267c5c6e
Charlieatinstinct <102666182+Charlieatinstinct@users.noreply.github.com>
2023-06-27 14:46:53 +0800

    :fire: [ios] 临时移除了IOS相关的代码

--- ios/BFExplorer/Plaoc/brige.m
+++
@@ -1,8 +0,0 @@
-//
-//  brige.m
-//  BFExplorer
-//
-//  Created by ui06 on 10/25/22.
-//
-
-#import <Foundation/Foundation.h>
--- ios/BFS_SwiftUI/BFS_SwiftUI/BFS_SwiftUI_Bridge.h
+++
@@ -1,6 +0,0 @@
-//
-//  BFS_SwiftUI_Bridge.h
-//  BFS_SwiftUI
-//
-//  Created by ui03 on 2023/5/27.
-//
