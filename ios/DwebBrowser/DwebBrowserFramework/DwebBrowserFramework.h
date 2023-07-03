e66fdf43ed1b1f8ab9c93ed3231e001a1bb46b9f
xing123456789 <516158548@qq.com>
2023-07-03 18:29:07 +0800

    fix: 添加framework.h文件

---
+++ ios/DwebBrowser/DwebBrowserFramework/DwebBrowserFramework.h
@@ -0,0 +1,18 @@
+//
+//  DwebBrowserFramework.h
+//  DwebBrowserFramework
+//
+//  Created by ui03 on 2023/7/3.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DwebBrowserFramework.
+FOUNDATION_EXPORT double DwebBrowserFrameworkVersionNumber;
+
+//! Project version string for DwebBrowserFramework.
+FOUNDATION_EXPORT const unsigned char DwebBrowserFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DwebBrowserFramework/PublicHeader.h>
+
+
