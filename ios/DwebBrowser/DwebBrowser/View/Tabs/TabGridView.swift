cc3b35d64f440267e6dc19b3603f60e57c499937
Charlieatinstinct <102666182+Charlieatinstinct@users.noreply.github.com>
2023-07-06 18:34:28 +0800

    :wrench: [iOS] changes the way to record of the cell frames from realtime to scroll ends, avoid mass record

--- ios/DwebBrowser/DwebBrowser/View/Tabs/TabGridView.swift
+++ ios/DwebBrowser/DwebBrowser/View/Tabs/TabGridView.swift
@@ -5,108 +5,131 @@
 //  Created by ui06 on 4/25/23.
 //
 
-import SwiftUI
 import Combine
+import SwiftUI
 
-struct CellFrameInfo: Equatable{
+struct CellFrameInfo: Equatable {
     var index: Int
     var frame: CGRect
 }
 
 struct CellFramePreferenceKey: PreferenceKey {
     static var defaultValue: [CellFrameInfo] = []
-    
+
     static func reduce(value: inout [CellFrameInfo], nextValue: () -> [CellFrameInfo]) {
         value += nextValue()
+    }
+}
+
+struct ViewOffsetKey: PreferenceKey {
+    static var defaultValue: CGFloat = 0
+
+    static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
+        value = nextValue()
     }
 }
 
 struct TabGridView: View {
     @EnvironmentObject var selectedTab: SelectedTab
     @EnvironmentObject var toolbarState: ToolBarState
-    
+
     @ObservedObject var cacheStore = WebCacheMgr.shared
     @ObservedObject var animation: ShiftAnimation
     @ObservedObject var gridState: TabGridState
 
+    @State var scrollingEnded: Bool = false
     @State var frames: [CellFrameInfo] = []
     @Binding var selectedCellFrame: CGRect
 
+    let detector = CurrentValueSubject<[CellFrameInfo], Never>([])
+    var publisher: AnyPublisher<[CellFrameInfo], Never> {
+        detector
+            .debounce(for: .seconds(0.05), scheduler: DispatchQueue.main)
+            .dropFirst()
+            .eraseToAnyPublisher()
+    }
+
     var body: some View {
         GeometryReader { geo in
-            ScrollViewReader{ scrollproxy in
-                ScrollView{
+            ScrollViewReader { scrollproxy in
+                ScrollView {
                     LazyVGrid(columns: [
-                        GridItem(.adaptive(minimum: (screen_width/3.0),maximum: screen_width/2.0), spacing: gridHSpace)
+                        GridItem(.adaptive(minimum: screen_width/3.0, maximum: screen_width/2.0), spacing: gridHSpace)
                     ], spacing: gridVSpace, content: {
                         ForEach(cacheStore.store, id: \.id) { webCache in
                             GridCell(webCache: webCache, isSelected: isSelected(webCache: webCache))
                                 .id(webCache.id)
                                 .background(GeometryReader { geometry in
-                                    Color.clear
-                                        .preference(key: CellFramePreferenceKey.self, value: [ CellFrameInfo(index: cacheStore.store.firstIndex(of: webCache) ?? 0, frame: geometry.frame(in: .global))])
+                                    Color.white
+                                        .preference(key: CellFramePreferenceKey.self,
+                                                    value: [CellFrameInfo(index: cacheStore.store.firstIndex(of: webCache) ?? 0, frame: geometry.frame(in: .global))])
                                 })
-                                .onAppear{
+                                .onAppear {
                                     if let index = WebCacheMgr.shared.store.firstIndex(of: webCache),
-                                       index == selectedTab.curIndex{
+                                       index == selectedTab.curIndex
+                                    {
                                         selectedCellFrame = cellFrame(at: index)
                                     }
                                 }
-                                
+
                                 .onTapGesture {
                                     guard let index = cacheStore.store.firstIndex(of: webCache) else { return }
                                     let currentFrame = cellFrame(at: index)
                                     let geoFrame = geo.frame(in: .global)
                                     print("\(geoFrame.minY) - \(currentFrame.minY), \(geoFrame.maxY) - \(currentFrame.maxY)")
-                                    if geoFrame.minY <= currentFrame.minY, geoFrame.maxY >= currentFrame.maxY{
+                                    if geoFrame.minY <= currentFrame.minY, geoFrame.maxY >= currentFrame.maxY {
                                         print("inside of grid")
-                                        if selectedTab.curIndex != index{
+                                        if selectedTab.curIndex != index {
                                             selectedTab.curIndex = index
                                         }
                                         selectedCellFrame = currentFrame
                                         toolbarState.showTabGrid = false
-                                    }else{
+                                    } else {
                                         print("outside of grid")
-                                        if selectedTab.curIndex != index{
+                                        if selectedTab.curIndex != index {
                                             selectedTab.curIndex = index
                                         }
-                                        withAnimation(.linear(duration: 0.2),{
-                                            scrollproxy.scrollTo(webCache.id,anchor: .center)
-                                        })
-                                        DispatchQueue.main.asyncAfter(deadline: .now()+0.25, execute: {
+                                        withAnimation(.linear(duration: 0.2)) {
+                                            scrollproxy.scrollTo(webCache.id, anchor: .center)
+                                        }
+                                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.25) {
                                             selectedCellFrame = currentFrame
                                             toolbarState.showTabGrid = false
-                                        })
+                                        }
                                     }
                                 }
                                 .shadow(color: Color.gray, radius: 6)
-                                
                         }
                     })
                     .padding(gridHSpace)
                     .onPreferenceChange(CellFramePreferenceKey.self) { newFrames in
-                        if toolbarState.showTabGrid{
-                            self.frames = newFrames
+                        if toolbarState.showTabGrid {
+                            detector.send(newFrames)
                         }
                     }
                 }
-                
+                .coordinateSpace(name: "scroll")
+                .onReceive(publisher) {
+                    self.frames = $0
+                    print("frozen frames: \($0)")
+                }
+
                 .background(Color(white: 0, opacity: 0.2))
                 .onChange(of: animation.progress) { progress in
-                    if progress == .preparingShrink{
-                        //设置grid为不可见的状态，并滚动到对应的位置
-                        let index =  selectedTab.curIndex
+                    if progress == .preparingShrink {
+                        // 设置grid为不可见的状态，并滚动到对应的位置
+                        let index = selectedTab.curIndex
                         let webCache = cacheStore.store[index]
                         gridState.scale = 1
                         gridState.opacity = 0.01
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.05){
-                            scrollproxy.scrollTo(webCache.id,anchor: .center)
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.05) {
+                            scrollproxy.scrollTo(webCache.id, anchor: .center)
                         }
-                        
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1){ //time for scroll to index
-                            guard let selectedFrame = self.frames.filter({$0.index == selectedTab.curIndex}).first?.frame else { return }
+
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) { // time for scroll to index
+                            guard let selectedFrame = self.frames.filter({ $0.index == selectedTab.curIndex }).first?.frame else { return }
                             selectedCellFrame = selectedFrame
-                          
+
                             gridState.scale = 0.8
                             gridState.opacity = 1
                             animation.progress = .startShrinking
@@ -116,8 +139,8 @@ struct TabGridView: View {
             }
         }
     }
-    
-    func isSelected(webCache: WebCache) -> Bool{
+
+    func isSelected(webCache: WebCache) -> Bool {
         cacheStore.store.firstIndex(of: webCache) == selectedTab.curIndex
     }
 
@@ -131,7 +154,6 @@ struct TabGridView: View {
         return .zero
     }
 }
-
 
 struct TabsCollectionView_Previews: PreviewProvider {
     static var previews: some View {
