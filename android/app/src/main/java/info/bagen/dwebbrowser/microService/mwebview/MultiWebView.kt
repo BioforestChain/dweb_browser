f8dc051c4366ead71ee92bcc9268f860f94629d9
jackie-yellow <hl19081555@gmail.com>
2023-07-26 16:57:51 +0800

    :ambulance: [Android] 修复编译异常

--- android/app/src/main/java/info/bagen/dwebbrowser/microService/mwebview/MultiWebView.kt
+++ android/app/src/main/java/info/bagen/dwebbrowser/microService/mwebview/MultiWebView.kt
@@ -14,55 +14,56 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.focusRequester
 import com.google.accompanist.web.WebView
 import kotlinx.coroutines.launch
-import org.dweb_browser.helper.Mmid
+import org.dweb_browser.helper.MMID
 
 @Composable
-fun MultiWebView(mmid: Mmid, viewItem: MultiWebViewController.MultiViewItem) {
-  key(viewItem.webviewId) {
-    val wc = MultiWebViewNMM.getCurrentWebViewController(mmid) ?: return@key
-    val nativeUiController = viewItem.nativeUiController.effect()
-    val state = viewItem.state
-    val navigator = viewItem.navigator
+fun MultiWebView(mmid: MMID, viewItem: MultiWebViewController.MultiViewItem) {
+  MultiWebViewNMM.getCurrentWebViewController(mmid)?.let { wc ->
+    key(viewItem.webviewId) {
+      val nativeUiController = viewItem.nativeUiController.effect()
+      val state = viewItem.state
+      val navigator = viewItem.navigator
 
-    val chromeClient = remember { MultiWebViewChromeClient(wc, viewItem, wc.isLastView(viewItem)) }
+      val chromeClient = remember { MultiWebViewChromeClient(wc, viewItem, wc.isLastView(viewItem)) }
 
-    BackHandler(true) {
-      if (chromeClient.closeWatcherController.canClose) {
-        viewItem.coroutineScope.launch {
-          chromeClient.closeWatcherController.close()
-        }
-      } else if (navigator.canGoBack) {
-        debugMultiWebView("NAV/${viewItem.webviewId}", "go back")
-        navigator.navigateBack()
-      } else {
-        viewItem.coroutineScope.launch {
-          wc.closeWebView(viewItem.webviewId)
+      BackHandler(true) {
+        if (chromeClient.closeWatcherController.canClose) {
+          viewItem.coroutineScope.launch {
+            chromeClient.closeWatcherController.close()
+          }
+        } else if (navigator.canGoBack) {
+          debugMultiWebView("NAV/${viewItem.webviewId}", "go back")
+          navigator.navigateBack()
+        } else {
+          viewItem.coroutineScope.launch {
+            wc.closeWebView(viewItem.webviewId)
+          }
         }
       }
-    }
 
-    Box(
-      modifier = Modifier.fillMaxSize()
-    ) {
-      val modifierPadding by nativeUiController.safeArea.outerAreaInsetsState
-      WebView(
-        state = state,
-        navigator = navigator,
-        modifier = Modifier
-          .fillMaxSize()
-          .focusRequester(nativeUiController.virtualKeyboard.focusRequester)
-          .padding(modifierPadding.asPaddingValues()),
-        factory = {
-          // 修复 activity 已存在父级时导致的异常
-          viewItem.webView.parent?.let { parentView ->
-            (parentView as ViewGroup).removeAllViews()
-          }
-          viewItem.webView
-        },
-        chromeClient = chromeClient,
-        captureBackPresses = false,
-      )
-      chromeClient.BeforeUnloadDialog()
+      Box(
+        modifier = Modifier.fillMaxSize()
+      ) {
+        val modifierPadding by nativeUiController.safeArea.outerAreaInsetsState
+        WebView(
+          state = state,
+          navigator = navigator,
+          modifier = Modifier
+            .fillMaxSize()
+            .focusRequester(nativeUiController.virtualKeyboard.focusRequester)
+            .padding(modifierPadding.asPaddingValues()),
+          factory = {
+            // 修复 activity 已存在父级时导致的异常
+            viewItem.webView.parent?.let { parentView ->
+              (parentView as ViewGroup).removeAllViews()
+            }
+            viewItem.webView
+          },
+          chromeClient = chromeClient,
+          captureBackPresses = false,
+        )
+        chromeClient.BeforeUnloadDialog()
+      }
     }
   }
 }
