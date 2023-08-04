9f649c19d88d8a8083074cde7b51194448b27edc
jackie-yellow <hl19081555@gmail.com>
2023-08-04 11:20:56 +0800

    :recycle: [Android-jmm] 调整Jmm安装完成后，点击打开应用时，需要唤醒Desk到前端

--- android/app/src/main/java/info/bagen/dwebbrowser/microService/browser/desk/DeskNMM.kt
+++ android/app/src/main/java/info/bagen/dwebbrowser/microService/browser/desk/DeskNMM.kt
@@ -2,6 +2,7 @@ package info.bagen.dwebbrowser.microService.browser.desk
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import info.bagen.dwebbrowser.App
 import info.bagen.dwebbrowser.microService.browser.jmm.EIpcEvent
 import info.bagen.dwebbrowser.microService.core.AndroidNativeMicroModule
@@ -191,22 +192,18 @@ class DesktopNMM : AndroidNativeMicroModule("desk.browser.dweb", "Desk") {
             },
           ).cors()
         )
-
-
-    /// 启动对应的Activity视图
-    App.startActivity(DesktopActivity::class.java) { intent ->
-      intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-      intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
-      // 不可以添加 Intent.FLAG_ACTIVITY_NEW_DOCUMENT ，否则 TaskbarActivity 就没发和 DesktopActivity 混合渲染、点击穿透
-      intent.putExtras(Bundle().apply {
-        putString("deskSessionId", deskSessionId)
-        putString("taskBarSessionId", taskBarSessionId)
-      })
-    }
-    val activity = deskController.waitActivityCreated()
-
     onActivity {
       // TODO 如果收到激活指令，应该将activity唤醒到最前端
+      /// 启动对应的Activity视图，如果在后端也需要唤醒到最前面，所以需要在AndroidManifest.xml 配置 launchMode 为 singleTask
+      App.startActivity(DesktopActivity::class.java) { intent ->
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+        // 不可以添加 Intent.FLAG_ACTIVITY_NEW_DOCUMENT ，否则 TaskbarActivity 就没发和 DesktopActivity 混合渲染、点击穿透
+        intent.putExtras(Bundle().apply {
+          putString("deskSessionId", deskSessionId)
+          putString("taskBarSessionId", taskBarSessionId)
+        })
+      }
     }
   }
 
--- android/app/src/main/java/info/bagen/dwebbrowser/microService/browser/jmm/JmmController.kt
+++ android/app/src/main/java/info/bagen/dwebbrowser/microService/browser/jmm/JmmController.kt
@@ -5,32 +5,31 @@ import kotlinx.coroutines.sync.withLock
 import org.dweb_browser.microservice.help.MMID
 import org.dweb_browser.microservice.ipc.helper.IpcEvent
 
-enum class EIpcEvent(val event:String){
-  State("state"),
-  Ready("ready"),
-  Activity("activity"),
-  Close("close")
+enum class EIpcEvent(val event: String) {
+  State("state"), Ready("ready"), Activity("activity"), Close("close")
 }
 
 class JmmController(private val jmmNMM: JmmNMM) {
 
   private val openLock = Mutex()
 
-
   fun hasApps(mmid: MMID) = jmmNMM.getApps(mmid) !== null
   fun getApp(mmid: MMID) = jmmNMM.getApps(mmid)
 
   suspend fun openApp(mmid: MMID) {
     openLock.withLock {
-        val (ipc) = jmmNMM.bootstrapContext.dns.connect(mmid)
+      val (ipc) = jmmNMM.bootstrapContext.dns.connect(mmid)
       debugJMM("openApp", "postMessage==>activity ${ipc.remote.mmid}")
       ipc.postMessage(IpcEvent.fromUtf8(EIpcEvent.Activity.event, ""))
+
+      val (deskIpc) = jmmNMM.bootstrapContext.dns.connect("desk.browser.dweb")
+      debugJMM("openApp", "postMessage==>activity desk.browser.dweb")
+      deskIpc.postMessage(IpcEvent.fromUtf8(EIpcEvent.Activity.event, ""))
     }
   }
 
   suspend fun closeApp(mmid: MMID) {
-      debugJMM("close APP", "postMessage==>close  $mmid")
-      jmmNMM.bootstrapContext.dns.close(mmid)
+    debugJMM("close APP", "postMessage==>close  $mmid")
+    jmmNMM.bootstrapContext.dns.close(mmid)
   }
-
 }
