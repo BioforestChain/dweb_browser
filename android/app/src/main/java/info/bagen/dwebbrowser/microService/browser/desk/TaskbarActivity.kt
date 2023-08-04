430cd110a21f8425dd0ecf03e7adc3133d19dc8d
Gaubee <GaubeeBangeel@Gmail.com>
2023-08-04 18:51:05 +0800

    :sparkles: [Android] 实现了点击浮窗外面自动关闭浮窗

--- android/app/src/main/java/info/bagen/dwebbrowser/microService/browser/desk/TaskbarActivity.kt
+++ android/app/src/main/java/info/bagen/dwebbrowser/microService/browser/desk/TaskbarActivity.kt
@@ -3,6 +3,7 @@ package info.bagen.dwebbrowser.microService.browser.desk
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.Gravity
+import android.view.MotionEvent
 import android.view.ViewGroup
 import android.view.WindowManager
 import androidx.activity.compose.BackHandler
@@ -33,7 +34,7 @@ class TaskbarActivity : BaseActivity() {
   @SuppressLint("UseCompatLoadingForDrawables")
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-     val taskBarController = bindController(intent.getStringExtra("taskBarSessionId"))
+    val taskBarController = bindController(intent.getStringExtra("taskBarSessionId"))
     val density = resources.displayMetrics.density
 
     setContent {
@@ -41,11 +42,11 @@ class TaskbarActivity : BaseActivity() {
       /// window.setLayout((80 * density).toInt(), (200 * density).toInt())
       val layoutWidth = maxOf(taskbarViewModel.width, (80 * density).toInt())
       val layoutHeight = maxOf(taskbarViewModel.height, (200 * density).toInt())
-      window.setLayout(layoutWidth , layoutHeight)
+      window.setLayout(layoutWidth, layoutHeight)
       window.attributes = window.attributes.also { attributes ->
-        /// 禁用模态窗口模式，使得点击可以向下穿透
-        attributes.flags =
-          WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS or WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
+//        /// 禁用模态窗口模式，使得点击可以向下穿透
+//        attributes.flags =
+//          WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
         /// 将视图对其到右侧中央
         attributes.gravity = Gravity.CENTER_VERTICAL or Gravity.END;
       }
@@ -59,7 +60,6 @@ class TaskbarActivity : BaseActivity() {
             webView.parent?.let { parent ->
               (parent as ViewGroup).removeView(webView)
             }
-
           }
         })
       }
@@ -68,7 +68,10 @@ class TaskbarActivity : BaseActivity() {
     /// 启用模糊
     blurHelper.config(
       backgroundBlurRadius = (10 * density).toInt(),
-      windowBackgroundDrawable = getDrawable(R.drawable.taskbar_window_background)
+      windowBackgroundDrawable = getDrawable(R.drawable.taskbar_window_background),
+      dimAmountNoBlur = 0.3f,
+      dimAmountWithBlur = 0.1f,
+//      blurBehindRadius = (5 * density).toInt(),
     )
   }
 
@@ -77,4 +80,13 @@ class TaskbarActivity : BaseActivity() {
     taskbarViewModel.floatViewState.value = true // 销毁 TaskbarActivity 后需要将悬浮框重新显示加载
   }
 
+  @SuppressLint("RestrictedApi")
+  override fun dispatchTouchEvent(ev: MotionEvent): Boolean {
+    with(ev) {
+      if (x <= 0 || y <= 0 || x >= window.decorView.width || y >= window.decorView.height) {
+        finish()
+      }
+    }
+    return super.dispatchTouchEvent(ev)
+  }
 }
