2621e7d5a5c1a963b4843391af7f4879ebdf18f0
kingsword09 <kingsword09@gmail.com>
2023-08-17 11:14:10 +0800

    :sparkles: [next C#] 暂时使用dynamic替代泛型来实现Observable

---
+++ next/dweb-browser/Helper/src/Observable.cs
@@ -0,0 +1,90 @@
+﻿namespace DwebBrowser.Helper;
+
+public class Observable
+{
+    public record Change(string Key, dynamic NewValue, dynamic OldValue);
+
+    public Listener<Change> Listener = new();
+
+    private readonly List<Observer> observers = new();
+    public List<Observer> Observers => observers.ToList();
+
+    public Observer Observe(string key, dynamic initValue)
+    {
+        var observer = new Observer(this, key, initValue);
+
+        observers.Add(observer);
+
+        observer.OnListener += async (v, ov, _) =>
+        {
+            if (v.Equals(ov))
+            {
+                return;
+            }
+
+            await Listener.Emit(new Change(key, v, ov));
+        };
+
+        return observer;
+    }
+
+    public Observer ObserveNullable(string key, dynamic initValue = null)
+    {
+        var observer = new Observer(this, key, initValue);
+
+        observers.Add(observer);
+
+        observer.OnListener += async (v, ov, _) =>
+        {
+            if (v.Equals(ov))
+            {
+                return;
+            }
+
+            await Listener.Emit(new Change(key, v, ov));
+        };
+
+        return observer;
+    }
+
+    public class Observer
+    {
+        private readonly HashSet<Signal<dynamic, dynamic>> signal = new();
+        public event Signal<dynamic, dynamic> OnListener
+        {
+            add { if (value != null) lock (signal) { signal.Add(value); } }
+            remove { lock (signal) { signal.Remove(value); } }
+        }
+
+        public dynamic Value { get; private set; }
+
+        public dynamic Get()
+        {
+            return Value;
+        }
+
+        public void Set(dynamic value)
+        {
+            if (Value.Equals(value))
+            {
+                return;
+            }
+
+            var oldValue = Value;
+            Value = value;
+
+            _ = Ob.Listener.Emit(new Change(Key, value, oldValue));
+        }
+
+        private Observable Ob { get; init; }
+        public string Key { get; init; }
+        public Observer(Observable ob, string key, dynamic initValue)
+        {
+            Ob = ob;
+            Key = key;
+            Value = initValue;
+
+            Ob.Observers.Add(this);
+        }
+    }
+}
---
+++ next/dweb-browser/Helper/tests/ObservableTest.cs
@@ -0,0 +1,84 @@
+﻿using System.Text.Json;
+
+namespace DwebBrowser.HelperTests;
+
+public class ObservableTest
+{
+    public class WindowState
+    {
+        public Observable observable = new();
+
+        public WindowState()
+        {
+            a = observable.Observe("A", false);
+            b = observable.Observe("B", new Base() { C = 1, D = false });
+        }
+
+        Observable.Observer a { get; init; }
+        public bool A
+        {
+            get => a.Get();
+            set => a.Set(value);
+        }
+
+        public record class Base
+        {
+            public int C { get; set; }
+            public bool D { get; set; }
+        }
+
+        Observable.Observer b { get; init; }
+        public Base B
+        {
+            get => b.Get();
+            set => b.Set(value);
+        }
+    }
+
+    [Fact]
+    public void ObservableSimpleTest()
+    {
+        var windowState = new WindowState();
+
+        windowState.observable.Listener.OnListener += async (change, _) =>
+        {
+            Debug.WriteLine(JsonSerializer.Serialize(windowState));
+        };
+
+        Debug.WriteLine(windowState.A);
+        windowState.A = true;
+        Debug.WriteLine(windowState.A);
+    }
+
+    [Fact]
+    public void ObservableClassTest()
+    {
+        var windowState = new WindowState();
+
+        windowState.observable.Listener.OnListener += async (change, _) =>
+        {
+            Debug.WriteLine($"2: {0}", JsonSerializer.Serialize(windowState));
+        };
+
+        Debug.WriteLine($"1: {0}", JsonSerializer.Serialize(windowState));
+        windowState.B = windowState.B with { C = 2 };
+        Debug.WriteLine($"3: {0}", JsonSerializer.Serialize(windowState));
+        windowState.A = true;
+    }
+
+    [Fact]
+    public void ObservableClassTestFail()
+    {
+        var windowState = new WindowState();
+
+        windowState.observable.Listener.OnListener += async (change, _) =>
+        {
+            // 没触发
+            Debug.WriteLine($"2: {0}", JsonSerializer.Serialize(windowState));
+        };
+
+        Debug.WriteLine($"1: {0}", JsonSerializer.Serialize(windowState));
+        windowState.B.C = 2;
+        Debug.WriteLine($"3: {0}", JsonSerializer.Serialize(windowState));
+    }
+}
