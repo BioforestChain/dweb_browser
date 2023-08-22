7ce5962dea2e2829a59f76013219b2dcfbcf50ff
jackie-yellow <hl19081555@gmail.com>
2023-08-22 16:13:00 +0800

    :ambulance: [Android] 紧急修复，userName需要增加过滤处理

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -61,7 +61,7 @@ android {
     debug {
       defaultConfig {
         val userName = System.getProperty("user.name")
-          .replace("[^a-zA-Z0-9]", "").lowercase().let { name -> // 获取系统名称,并且只保留了英文和数字,再转小写
+          .replace("[^a-zA-Z0-9]".toRegex(), "").lowercase().let { name -> // 获取系统名称,并且只保留了英文和数字,再转小写
             if (name.first() in '0'..'9') { // 判断第一个是否是数字，如果是数字，前面增加一个英文a
               "a$name"
             } else {
