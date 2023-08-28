30f4e07594c81c5a8559b9e6c7f752f8c9bac577
waterbang <waterbang@qq.com>
2023-08-28 19:46:59 +0800

    :bookmark: [plaoc/cli] publish version 0.2.0 for plaoc/cli

--- plaoc/cli/plaoc.ts
+++ plaoc/cli/plaoc.ts
@@ -3,13 +3,14 @@ import { doConfigFlags } from "./config.ts";
 import { Command } from "./deps.ts";
 import { doServeFlags } from "./serve.ts";
 
-const cliNpm = import.meta.resolve(`../scripts/npm.cli.json`);
-const npmConfigs = (await import(cliNpm, { assert: { type: "json" } })).default;
+//node --trace-warnings
+// const cliNpm = import.meta.resolve(`../scripts/npm.cli.json`);
+// const npmConfigs = (await import(cliNpm, { assert: { type: "json" } })).default;
 
 await new Command()
   .name("@plaoc/cli")
   .description("plaoc front-end and back-end packaging tools.")
-  .version(`v${npmConfigs[0].version}`)
+  .version(`v0.2.0`)
   .command("serve", doServeFlags)
   .example("developer service", "plaoc serve http://xx.xx.xx.xx:xxxx/")
 
