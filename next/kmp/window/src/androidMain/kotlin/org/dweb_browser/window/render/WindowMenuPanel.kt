684e18678e0c2839258162875b498116556e0c01
Gaubee <GaubeeBangeel@Gmail.com>
2023-09-18 22:01:53 +0800

    :recycle: [kmp/window] 移植使用WindowMenuPanelByAlert替代RichTooltip方案，修复Parcelable的问题

--- next/kmp/window/src/androidMain/kotlin/org/dweb_browser/window/render/WindowMenuPanel.kt
+++ next/kmp/window/src/androidMain/kotlin/org/dweb_browser/window/render/WindowMenuPanel.kt
@@ -10,6 +10,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.RichTooltip
@@ -29,9 +32,16 @@ import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
 import org.dweb_browser.window.core.WindowController
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 internal fun WindowMenuPanel(
+  win: WindowController,
+) {
+  WindowMenuPanelByAlert(win)
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+internal fun WindowMenuPanelByRichTooltip(
   win: WindowController,
 ) {
   val winPadding = LocalWindowPadding.current
@@ -112,3 +122,70 @@ internal fun WindowMenuPanel(
   }
 }
 
+
+@Composable
+fun WindowControllerTheme.toButtonColors() = ButtonDefaults.buttonColors(
+  contentColor = themeContentColor,
+  containerColor = themeColor,
+  disabledContentColor = themeContentDisableColor
+)
+
+@Composable
+internal fun WindowMenuPanelByAlert(
+  win: WindowController,
+) {
+  val scope = rememberCoroutineScope()
+  val isShowMenuPanel by win.watchedState { showMenuPanel }
+  val toggleMenu = { show: Boolean ->
+    scope.launch {
+      win.toggleMenuPanel(show)
+    }
+  }
+  if (isShowMenuPanel) {
+    val winTheme = LocalWindowControllerTheme.current
+    val buttonColors = winTheme.toButtonColors()
+    AlertDialog(
+      onDismissRequest = {
+        toggleMenu(false)
+      },
+      containerColor = winTheme.themeColor,
+      iconContentColor = winTheme.themeContentColor,
+      textContentColor = winTheme.themeContentColor,
+      titleContentColor = winTheme.themeContentColor,
+      icon = {
+        win.IconRender(modifier = Modifier.size(36.dp))
+      },
+      title = {
+        val owner = win.state.constants.owner
+        Text(text = owner, maxLines = 1, overflow = TextOverflow.Ellipsis)
+      },
+      text = {
+        WindowControlPanel(win)
+      },
+      confirmButton = {
+        ElevatedButton(
+          onClick = {
+            scope.launch {
+              win.hideMenuPanel()
+              win.close() // 增加关闭窗口
+            }
+          },
+          colors = buttonColors,
+        ) {
+          Text("退出应用")
+        }
+      },
+      dismissButton = {
+        Button(
+          onClick = {
+            toggleMenu(false)
+          },
+          colors = buttonColors,
+        ) {
+          Text("关闭面板")
+        }
+      },
+    )// AlertDialog
+
+  }
+}
