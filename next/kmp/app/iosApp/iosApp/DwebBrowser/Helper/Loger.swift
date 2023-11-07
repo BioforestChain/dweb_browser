2a3685e2fae444865528540775cf93ea21a7b58a
Mike <727593012@qq.com>
2023-11-07 18:29:29 +0800

    [iOS] 添加log

---
+++ next/kmp/app/iosApp/iosApp/DwebBrowser/Helper/Loger.swift
@@ -0,0 +1,27 @@
+//
+//  Loger.swift
+//  iosApp
+//
+//  Created by instinct on 2023/11/7.
+//  Copyright © 2023 orgName. All rights reserved.
+//
+
+import Foundation
+
+private var dateformate: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateFormat = "HH:mm:ss:SSSS"
+    return formatter
+}()
+
+func Log(time: String = dateformate.string(from: Date()), 
+         file: String = #file,
+         function: String = #function,
+         line: Int = #line,
+         category: String = "Debug",
+         _ msg: String?) {
+#if DEBUG
+    print("[\(time)] [\(file.components(separatedBy: "/").last ?? "null"):\(function):\(line)]\n[\(category)] \(msg ?? "")")
+#else
+#endif
+}
