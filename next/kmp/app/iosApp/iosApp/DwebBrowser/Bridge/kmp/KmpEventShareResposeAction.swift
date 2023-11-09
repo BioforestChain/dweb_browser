60675085b72c4dc560d88ade30254c72a006cd5a
Mike <727593012@qq.com>
2023-11-09 18:49:37 +0800

    [kmp/iOS] KmpNativeBridge去掉callback参数。

--- next/kmp/app/iosApp/iosApp/DwebBrowser/Bridge/kmp/KmpEventShareResposeAction.swift
+++ next/kmp/app/iosApp/iosApp/DwebBrowser/Bridge/kmp/KmpEventShareResposeAction.swift
@@ -8,19 +8,9 @@
 
 import UIKit
 
-class KmpEventShareResposeAction: KmpEventResposeActionProtocol {
+class KmpEventShareResposeAction: KmpEventResposeAction {
     
-    init(eventName: String) {
-        self.eventName = eventName
-    }
-    
-    let eventName:String
-    
-    func inputDatas() -> [Any] {
-        guard let event = KmpBridgeManager.shared.event(for: KmpEvent.share) else {
-            return []
-        }
-        
+    override func parseInputDatas() -> [Any]? {
         var result = [Any]()
         
         if let title = event.inputDatas?["title"] {
@@ -35,32 +25,29 @@ class KmpEventShareResposeAction: KmpEventResposeActionProtocol {
         if let files = event.inputDatas?["files"] as? [Data]{
             result.append(contentsOf: files)
         }
-        
         return result
     }
-    
-    typealias KmpStringUnitType = @convention(block) (String) -> Void
-    
-    func outputDatas() -> ((String)->Void)? {
-        guard let event = KmpBridgeManager.shared.event(for: KmpEvent.share) else {
-            return nil
+
+    @MainActor
+    override func doAction() async -> Any? {
+        let controller = UIActivityViewController(activityItems: parseInputDatas() ?? [], applicationActivities: nil)
+        UIApplication.shared.keyWindow?.rootViewController!.present(controller, animated: true, completion: nil)
+        return await withCheckedContinuation { continuation  in
+            controller.completionWithItemsHandler = { _, completed, _, error in
+                continuation.resume(returning: completed ? "OK" : (error?.localizedDescription ?? "Cancel"))
+            }
         }
-        guard let callback = event.outputDatas?["callback"] else { return nil }
-        // FIXME: 此处比较危险，直接避开编译器的安全检查，直接强转为swift的closure. 后期看情况是否需要优化
-        let handler = unsafeBitCast(callback as AnyObject, to: KmpStringUnitType.self)
-        return handler
-    }
-    
-    func doResponseAction() {
-        let completedHandler: UIActivityViewController.CompletionWithItemsHandler = { [weak self] _, completed, _, error in
-            guard let self = self, let callback = self.outputDatas() else { return }
-            let msg = completed ? "OK" : (error?.localizedDescription ?? "Cancel")
-            callback(msg)
-            KmpBridgeManager.shared.complete(for: KmpEvent.share)
-        }
-        
-        let activityController = UIActivityViewController(activityItems: inputDatas(), applicationActivities: nil)
-        activityController.completionWithItemsHandler = completedHandler
-        UIApplication.shared.keyWindow?.rootViewController!.present(activityController, animated: true, completion: nil)
     }
 }
+
+//    typealias KmpStringUnitType = @convention(block) (String) -> Void
+//
+//    func outputDatas() -> ((String)->Void)? {
+//        guard let event = KmpBridgeManager.shared.event(for: KmpEvent.share) else {
+//            return nil
+//        }
+//        guard let callback = event.outputDatas?["callback"] else { return nil }
+//        // FIXME: 此处比较危险，直接避开编译器的安全检查，直接强转为swift的closure. 后期看情况是否需要优化
+//        let handler = unsafeBitCast(callback as AnyObject, to: KmpStringUnitType.self)
+//        return handler
+//    }
--- next/kmp/app/iosApp/iosApp/iOSApp.swift
+++ next/kmp/app/iosApp/iosApp/iOSApp.swift
@@ -29,9 +29,6 @@ struct iOSApp: App {
             .onReceive(networkManager.$isNetworkAvailable) { isAvailable in
                 isNetworkSegmentViewPresented = !isAvailable
             }
-            .onReceive(KmpBridgeManager.shared.eventPublisher.filter{ $0.name == KmpEvent.share }, perform: { event in
-                event.responseAction?.doResponseAction()
-            })
 		}
 	}
 }
