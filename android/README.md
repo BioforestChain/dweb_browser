0e3889007ef9ff6f6fa194622aae1d5c5c81ebd1
jackie-yellow <hl19081555@gmail.com>
2023-11-10 17:20:31 +0800

    :fire: [android] 删除android目录，已全部迁移到kmp

--- android/README.md
+++
@@ -1,1 +0,0 @@
-# dweb_browser for Android
--- android/app/src/main/java/info/bagen/dwebbrowser/microService/sys/fileSystem/exeprions/DirectoryExistsException.kt
+++
@@ -1,7 +0,0 @@
-package info.bagen.dwebbrowser.microService.sys.fileSystem.exeprions
-
-class DirectoryExistsException : Exception {
-  constructor(s: String?) : super(s) {}
-  constructor(t: Throwable?) : super(t) {}
-  constructor(s: String?, t: Throwable?) : super(s, t) {}
-}
