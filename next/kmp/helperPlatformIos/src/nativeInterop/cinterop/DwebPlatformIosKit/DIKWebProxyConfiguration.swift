c66ddf591f89fa4cfea51fe096990720d64e44f2
Mike <727593012@qq.com>
2023-11-20 15:02:38 +0800

    [iOS] api更换名字。

--- next/kmp/helperPlatformIos/src/nativeInterop/cinterop/DwebPlatformIosKit/DIKWebProxyConfiguration.swift
+++ next/kmp/helperPlatformIos/src/nativeInterop/cinterop/DwebPlatformIosKit/DIKWebProxyConfiguration.swift
@@ -10,7 +10,7 @@ import WebKit
 
 @objc public class DIKWebProxyConfiguration: NSObject {
     
-    @objc public func createConfiguration(url: String, port: UInt16) -> WKWebsiteDataStore {
+    @objc public func createWKWebsiteDataStore(url: String, port: UInt16) -> WKWebsiteDataStore {
         let endpoint = NWEndpoint.hostPort(host: NWEndpoint.Host(url) , port: NWEndpoint.Port(rawValue: port)!)
         var proxyConfig = ProxyConfiguration.init(httpCONNECTProxy: endpoint, tlsOptions: .none)
         let websiteDataStore = WKWebsiteDataStore.default()
