ef05921adf2b0a1de7e9f5fb987dc73500c91bdf
Mike <727593012@qq.com>
2023-11-20 17:20:02 +0800

    [iOS] wkview的proxy 改用OC写法。

--- next/kmp/helperPlatformIos/src/nativeInterop/cinterop/DwebPlatformIosKit/DwebPlatformIosKit.h
+++ next/kmp/helperPlatformIos/src/nativeInterop/cinterop/DwebPlatformIosKit/DwebPlatformIosKit.h
@@ -16,3 +16,4 @@ FOUNDATION_EXPORT const unsigned char DwebPlatformIosKitVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <DwebPlatformIosKit/PublicHeader.h>
 
 
+#import <DwebPlatformIosKit/DIPOCProxyConfiguration.h>
