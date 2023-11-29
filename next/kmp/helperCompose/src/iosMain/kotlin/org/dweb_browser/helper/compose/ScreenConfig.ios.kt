227e00e453aabc6f8f22921125f162273154a5e1
jackie-yellow <hl19081555@gmail.com>
2023-11-29 09:54:41 +0800

    :ambulance:

--- next/kmp/helperCompose/src/iosMain/kotlin/org/dweb_browser/helper/compose/ScreenConfig.ios.kt
+++ next/kmp/helperCompose/src/iosMain/kotlin/org/dweb_browser/helper/compose/ScreenConfig.ios.kt
@@ -1,8 +1,10 @@
 package org.dweb_browser.helper.compose
 
 import androidx.compose.runtime.Composable
+import org.dweb_browser.helper.WARNING
 
 @Composable
 actual fun rememberScreenSize(): ScreenSize {
+  WARNING("Not yet implemented rememberScreenSize")
   return ScreenSize.Zero
 }
