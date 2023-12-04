15a34d34c4bc6aa84a63485637ddc2db0fa32867
kingsword09 <kingsword09@gmail.com>
2023-12-04 15:12:33 +0800

    :sparkles: [plaoc/is-dweb] 0.0.3 release

--- plaoc/src/is-dweb/main.ts
+++ plaoc/src/is-dweb/main.ts
@@ -1,8 +1,18 @@
-
 export const isDweb = () => {
-  const isDweb = self.navigator.userAgent.includes("dweb")
+  const isDweb = self.navigator.userAgent.includes("Dweb");
   // @ts-ignore
-  const isPlaoc = self.__native_close_watcher_kit__ !== void 0
-  return isDweb || isPlaoc
-}
+  const isPlaoc = self.__native_close_watcher_kit__ !== void 0;
+  return isDweb || isPlaoc;
+};
 
+export const dwebTarget = () => {
+  if (isDweb()) {
+    const matches = self.navigator.userAgent.match(/Dweb\/([\d\.]+)/);
+    
+    if (matches && matches.length > 1) {
+      return parseFloat(matches[1]);
+    }
+  }
+
+  return 1.0;
+};
