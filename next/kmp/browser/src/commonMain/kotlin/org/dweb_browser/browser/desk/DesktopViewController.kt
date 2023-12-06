f52130088fd2d56461417c3b7e733a0589ce2f4e
jackie-yellow <hl19081555@gmail.com>
2023-12-06 18:56:42 +0800

    :bug: [kmp/desk] 修复 TaskbarActivity 关闭时，taskbarFloatView 没有重新唤醒的问题。

--- next/kmp/browser/src/commonMain/kotlin/org/dweb_browser/browser/desk/DesktopViewController.kt
+++ next/kmp/browser/src/commonMain/kotlin/org/dweb_browser/browser/desk/DesktopViewController.kt
@@ -2,8 +2,13 @@ package org.dweb_browser.browser.desk
 
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 import org.dweb_browser.helper.Rect
 import org.dweb_browser.helper.platform.IPureViewBox
 import org.dweb_browser.helper.platform.IPureViewController
@@ -26,15 +31,27 @@ class DesktopViewControllerCore(val controller: IPureViewController) {
       controllers.activityPo.resolve(IPureViewBox.from(controller))
     } ?: throw Exception("no found controller by sessionId: $sessionId")
   }
+  private val resumeState = mutableStateOf(false) // 增加字段，为了恢复 taskbarFloatView
 
   init {
     controller.onCreate { params ->
       val (desktopController, taskbarController, microModule) = bindController(params.getString("deskSessionId"))
       controller.addContent {
         DwebBrowserAppTheme {
+          LaunchedEffect(resumeState) {
+            snapshotFlow { resumeState.value }.collect {
+              // 增加字段，为了恢复 taskbarFloatView
+              if (it) taskbarController.toggleFloatWindow(false)
+              resumeState.value = false
+            }
+          }
           desktopController.Render(taskbarController, microModule)
         }
       }
+    }
+
+    controller.onResume {
+      resumeState.value = true
     }
 
     controller.onDestroy {
