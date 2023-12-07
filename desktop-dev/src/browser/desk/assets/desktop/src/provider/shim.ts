63190001f54a22fa938f437a87437e76fac901e8
Gaubee <GaubeeBangeel@Gmail.com>
2023-12-07 16:56:04 +0800

    :bug: [desktop] 修复_is_native_support_context_menu_的判断机制

--- desktop-dev/src/browser/desk/assets/desktop/src/provider/shim.ts
+++ desktop-dev/src/browser/desk/assets/desktop/src/provider/shim.ts
@@ -2,12 +2,13 @@ import { CloseWatcher } from "../../../../../../../../plaoc/src/client/component
 export { CloseWatcher };
 export type $CloseWatcher = InstanceType<typeof CloseWatcher>;
 
+const mock_symbole = Symbol("moke-context-menu");
 let _is_native_support_context_menu_ = false;
-document.body.addEventListener("contextmenu", function test() {
-  _is_native_support_context_menu_ = true;
+document.body.addEventListener("contextmenu", function test(e) {
+  _is_native_support_context_menu_ = !(mock_symbole in e);
   document.body.removeEventListener("contextmenu", test);
 });
 export function dispatchContextMenuEvent(e: PointerEvent) {
   if (_is_native_support_context_menu_) return;
-  e.target?.dispatchEvent(new PointerEvent("contextmenu", e));
+  e.target?.dispatchEvent(Object.assign(new PointerEvent("contextmenu", e), { [mock_symbole]: true }));
 }
