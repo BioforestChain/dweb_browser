461c9f6e77b660c32cfe57af5bf65c491372edac
Gaubee <GaubeeBangeel@Gmail.com>
2023-12-07 16:26:53 +0800

    :bug: [desktop] 修复在Android上两次触发ContextMenuEvent的问题

--- desktop-dev/src/browser/desk/assets/desktop/src/provider/shim.ts
+++ desktop-dev/src/browser/desk/assets/desktop/src/provider/shim.ts
@@ -2,6 +2,12 @@ import { CloseWatcher } from "../../../../../../../../plaoc/src/client/component
 export { CloseWatcher };
 export type $CloseWatcher = InstanceType<typeof CloseWatcher>;
 
+let _is_native_support_context_menu_ = false;
+document.body.addEventListener("contextmenu", function test() {
+  _is_native_support_context_menu_ = true;
+  document.body.removeEventListener("contextmenu", test);
+});
 export function dispatchContextMenuEvent(e: PointerEvent) {
+  if (_is_native_support_context_menu_) return;
   e.target?.dispatchEvent(new PointerEvent("contextmenu", e));
 }
