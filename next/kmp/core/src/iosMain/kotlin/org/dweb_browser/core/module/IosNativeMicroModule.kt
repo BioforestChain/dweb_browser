a349be59b80720b5ab7871a8dd6dff8c3faab519
waterbang <waterbang@qq.com>
2023-12-07 09:22:56 +0800

    :ambulance:

--- next/kmp/core/src/iosMain/kotlin/org/dweb_browser/core/module/IosNativeMicroModule.kt
+++ next/kmp/core/src/iosMain/kotlin/org/dweb_browser/core/module/IosNativeMicroModule.kt
@@ -3,12 +3,9 @@ package org.dweb_browser.core.module
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
-import org.dweb_browser.helper.platform.IPureViewController
 import org.dweb_browser.helper.platform.NativeViewController.Companion.nativeViewController
 import org.dweb_browser.helper.platform.PureViewController
-import org.dweb_browser.helper.platform.PureViewCreateParams
 import platform.UIKit.UIApplication
-import kotlin.reflect.KClass
 
 lateinit var nativeMicroModuleUIApplication: UIApplication
 
@@ -19,7 +16,7 @@ val lockActivityState = Mutex()
 fun NativeMicroModule.startUIViewController(pureViewController: PureViewController) {
   ioAsyncScope.launch {
     lockActivityState.withLock {
-      if (grant?.waitPromise() == false) {
+      if (grant?.await() == false) {
         return@withLock // TODO 用户拒绝协议应该做的事情
       }
       nativeViewController.addOrUpdate(pureViewController)
