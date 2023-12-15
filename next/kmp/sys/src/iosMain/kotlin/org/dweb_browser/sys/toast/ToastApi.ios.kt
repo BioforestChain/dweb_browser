bc8b1a53da15864367c8133a6fdc0178a0d63e5b
kingsword09 <kingsword09@gmail.com>
2023-12-15 10:47:42 +0800

    :sparkles: [ios] 实现toast

--- next/kmp/sys/src/iosMain/kotlin/org/dweb_browser/sys/toast/ToastApi.ios.kt
+++ next/kmp/sys/src/iosMain/kotlin/org/dweb_browser/sys/toast/ToastApi.ios.kt
@@ -1,7 +1,61 @@
 package org.dweb_browser.sys.toast
 
-import org.dweb_browser.helper.WARNING
+import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.cinterop.useContents
+import org.dweb_browser.core.module.NativeMicroModule
+import org.dweb_browser.core.module.getUIApplication
+import org.dweb_browser.helper.withMainContext
+import platform.CoreGraphics.CGFloat
+import platform.CoreGraphics.CGRectMake
+import platform.CoreGraphics.CGSizeMake
+import platform.UIKit.UIColor
+import platform.UIKit.UILabel
+import platform.UIKit.UITextAlignmentCenter
+import platform.UIKit.UIView
+import platform.UIKit.UIViewAnimationOptionCurveEaseInOut
+import platform.UIKit.bottomLayoutGuide
+import platform.UIKit.topLayoutGuide
 
+@OptIn(ExperimentalForeignApi::class)
 actual suspend fun showToast(text: String, durationType: DurationType, positionType: PositionType) {
-  WARNING("Not yet implemented showToast")
+  withMainContext {
+    val toastLabel = UILabel()
+    toastLabel.setText(text)
+    toastLabel.setTextColor(UIColor.whiteColor)
+    toastLabel.setBackgroundColor(UIColor.blackColor.colorWithAlphaComponent(0.8))
+    toastLabel.setTextAlignment(UITextAlignmentCenter)
+
+    toastLabel.layer.setCornerRadius(10.0)
+    toastLabel.layer.setMasksToBounds(true)
+    val controller = NativeMicroModule.getUIApplication().keyWindow?.rootViewController
+
+    if (controller != null) {
+      val width = controller.view.frame.useContents {
+        size.width
+      } - 40
+      val height = toastLabel.sizeThatFits(CGSizeMake(width, CGFloat.MAX_VALUE)).useContents {
+        height
+      } + 20
+
+      val x = (controller.view.frame.useContents { size.width } - width) / 2
+      val y = when (positionType) {
+        PositionType.TOP -> controller.topLayoutGuide.length + 20
+        PositionType.CENTER -> (controller.view.frame.useContents { size.height } - height) / 2
+        PositionType.BOTTOM -> controller.view.frame.useContents { size.height } - controller.bottomLayoutGuide.length - height - 30
+      }
+
+      val duration = when(durationType) {
+        DurationType.SHORT -> 1.0
+        DurationType.LONG -> 3.0
+      }
+
+      toastLabel.setFrame(CGRectMake(x, y, width, height))
+      controller.view.addSubview(toastLabel)
+      UIView.animateWithDuration(duration = duration, delay = duration, UIViewAnimationOptionCurveEaseInOut, { toastLabel.setAlpha(0.0) }) { finished ->
+        if(finished) {
+          toastLabel.removeFromSuperview()
+        }
+      }
+    }
+  }
 }
