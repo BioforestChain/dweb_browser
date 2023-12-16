92e5a80bb76b32ee45abf0a0247b2d04f348b207
kingsword09 <kingsword09@gmail.com>
2023-12-16 17:14:56 +0800

    :wrench: [plaoc] plaoc task pub 现在默认可以发布全部的包，也可以手动指定一些包

--- plaoc/scripts/npm-pub.ts
+++ plaoc/scripts/npm-pub.ts
@@ -1,9 +1,31 @@
 import { whichSync } from "../../scripts/helper/WhichCommand.ts";
+import { colors } from "../cli/deps.ts";
 import { cwdResolve } from "./cwd.ts";
 
-export const doPubCore = async (cwd: string) => {
-  const cmdWhich = whichSync("npm")
-  const npm_cmd = new Deno.Command(cmdWhich!, {
+export const doPubCore = async (cwd: string, autoCheck = false) => {
+  const cmdWhich = whichSync("npm")!;
+  if (autoCheck) {
+    const packageJson = (
+      await import(cwdResolve(cwd + "/package.json"), {
+        assert: { type: "json" },
+      })
+    ).default;
+    const output = await new Deno.Command(cmdWhich, {
+      cwd,
+      args: ["info", packageJson.name, "--json"],
+      stdout: "piped",
+    })
+      .spawn()
+      .output();
+    const packageInfo = JSON.parse(new TextDecoder().decode(output.stdout));
+    if (packageInfo.versions.includes(packageJson.version)) {
+      console.warn(
+        colors.yellow(`package ${colors.cyan(packageJson.name)}${colors.dim('@')}${colors.green(packageJson.version)} already exists`)
+      );
+      return false;
+    }
+  }
+  const npm_cmd = new Deno.Command(cmdWhich, {
     cwd,
     args: ["publish", "--access", "public"],
     stdout: "inherit",
@@ -15,14 +37,18 @@ export const doPubCore = async (cwd: string) => {
   return status.success;
 };
 
+/**
+ * 将 package.json 的 version 同步到 npm.*.json 中
+ */
 export const doPubFromJson = async (
   inputConfigFile: string,
-  outputConfigFile = /^http(s{0,1})\:\/\//.test(inputConfigFile) ? undefined : inputConfigFile
+  outputConfigFile = /^http(s{0,1})\:\/\//.test(inputConfigFile) ? undefined : inputConfigFile,
+  autoCheck: boolean
 ) => {
   const npmConfigs = (await import(inputConfigFile, { assert: { type: "json" } })).default;
 
   for (const config of npmConfigs) {
-    if (await doPubCore(config.buildToRootDir)) {
+    if (await doPubCore(config.buildToRootDir, autoCheck)) {
       /// 更新配置文件
       config.version = (
         await import(cwdResolve(config.buildToRootDir + "/package.json"), {
@@ -42,8 +68,15 @@ export const doPubFromJson = async (
 };
 
 export const doPub = async (args = Deno.args) => {
-  const target = args[0] ?? "client";
-  await doPubFromJson(import.meta.resolve(`./npm.${target}.json`));
+  /**
+   * 如果不传入任何参数，那么就自动检测版本
+   */
+  const autoCheck = args.length === 0;
+  const targets = args.length === 0 ? ["client", "cli", "server", "is-dweb"] : args;
+  for (const target of targets) {
+    console.log(colors.blue(`start pub ${colors.cyan(target)}:`))
+    await doPubFromJson(import.meta.resolve(`./npm.${target}.json`), undefined, autoCheck);
+  }
 };
 
 if (import.meta.main) {
