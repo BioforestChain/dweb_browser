4e9c8d7c8b03a6481a030e58fad15d96a73df5c3
kingsword09 <kingsword09@gmail.com>
2023-12-16 16:52:31 +0800

    :bug: [plaoc] 修复cli的version不是读取package.json的问题

--- plaoc/cli/plaoc.ts
+++ plaoc/cli/plaoc.ts
@@ -1,3 +1,5 @@
+import fs from "node:fs";
+import url from "node:url";
 import { doBundleCommand } from "./bundle.ts";
 import { doConfigCommand } from "./config.ts";
 import { Command } from "./deps.ts";
@@ -8,11 +10,30 @@ import { doWebAdvCommand } from "./web-dav.ts";
 //node --trace-warnings
 // const cliNpm = import.meta.resolve(`../scripts/npm.cli.json`);
 // const npmConfigs = (await import(cliNpm, { assert: { type: "json" } })).default;
+const findPackageJson = async () => {
+  let deep = 1;
+  let cur = import.meta.url;
+  let pre = "";
+  while (pre !== cur) {
+    try {
+      const newCur = await import.meta.resolve(`${"../".repeat(deep)}package.json`);
+      pre = cur;
+      cur = newCur;
+      // return await import(cur, { assert: { type: "json" } });
+      return JSON.parse(fs.readFileSync(url.fileURLToPath(cur), "utf-8"));
+    } catch {
+      deep++;
+    }
+  }
+  return { version: "0.0.0-dev" };
+};
+const packageJson = await findPackageJson();
+const packageVersion = packageJson.version;
 
 await new Command()
   .name("@plaoc/cli")
   .description("plaoc front-end and back-end packaging tools.")
-  .version(`v0.6.4`)
+  .version(`v${packageVersion}`)
   .command("serve", doServeCommand)
   .example("developer service", "plaoc serve http://xx.xx.xx.xx:xxxx/")
 
@@ -21,14 +42,10 @@ await new Command()
 
   .command("config", doConfigCommand)
   .example("set webadv-server-url", "plaoc config set webadv-server-url http://xx.xx.xx.xx:xxxx/")
-  .example("set webadv-server-auth","plaoc config set webadv-server-auth xxx")
+  .example("set webadv-server-auth", "plaoc config set webadv-server-auth xxx")
 
-  
   .command("run", doWebAdvCommand)
-  .example(
-    "deploy web adv server",
-    "plaoc run webadv-server -p 7777 -host 0.0.0.0 --download-dir /usr/xxx/"
-  )
+  .example("deploy web adv server", "plaoc run webadv-server -p 7777 -host 0.0.0.0 --download-dir /usr/xxx/")
   //TODO
   // .command("bundle-web-hook-server", doHookServer)
   // .example(
