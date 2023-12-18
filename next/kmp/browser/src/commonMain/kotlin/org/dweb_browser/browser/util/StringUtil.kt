9a9b51be7507740e374fdcef5e0191c645dfa2c7
jackie-yellow <hl19081555@gmail.com>
2023-12-18 10:02:30 +0800

    :bug: [kmp/browser] 修复url和host的判断问题。

--- next/kmp/browser/src/commonMain/kotlin/org/dweb_browser/browser/util/StringUtil.kt
+++ next/kmp/browser/src/commonMain/kotlin/org/dweb_browser/browser/util/StringUtil.kt
@@ -7,19 +7,23 @@ fun String.isUrl(): Boolean {
   // 以 http 或者 https 或者 ftp 打头，可以没有
   // 字符串中只能包含数字和字母，同时可以存在-
   // 最后以 2~5个字符 结尾，可能还存在端口信息，端口信息限制数字，长度为1~5位
-  val regex =
-    "^((https?|ftp)://)?([a-zA-Z0-9]+([-.][a-zA-Z0-9]+)*\\.[a-zA-Z]{2,5}(:[0-9]{1,5})?(/.*)?)$".toRegex()
-  return regex.matches(this)
-}
-
-fun String.isHost(): Boolean {
+  val regex1 =
+    "^((https?|ftp)://)([a-zA-Z0-9]+([-.][a-zA-Z0-9]+)*\\.[a-zA-Z]{2,5}(:[0-9]{1,5})?(/.*)?)$".toRegex()
   // 只判断 host(长度1~63,结尾是.然后带2~6个字符如[.com]，没有端口判断)：val regex = "^((?!-)[A-Za-z0-9-]{1,63}(?<!-)\\.)+[A-Za-z]{2,6}\$".toRegex()
-  val regex =
-    "((https?|ftp)://)(((2(5[0-5]|[0-4]\\d))|[0-1]?\\d{1,2})(\\.((2(5[0-5]|[0-4]\\d))|[0-1]?\\d{1,2})){3}(:[0-9]{1,5})?(/.*)?)".toRegex()
-  return regex.matches(this)
+  val regex2 =
+    "^((https?|ftp)://)(((2(5[0-5]|[0-4]\\d))|[0-1]?\\d{1,2})(\\.((2(5[0-5]|[0-4]\\d))|[0-1]?\\d{1,2})){3}(:[0-9]{1,5})?(/.*)?)$".toRegex()
+  return regex1.matches(this) || regex2.matches(this)
 }
 
-fun String.isUrlOrHost() = this.isUrl() || this.isHost()
+fun String.isOnlyHost(): Boolean {
+  val regex1 =
+    "^([a-zA-Z0-9]+([-.][a-zA-Z0-9]+)*\\.[a-zA-Z]{2,5}(:[0-9]{1,5})?(/.*)?)$".toRegex()
+  val regex2 =
+    "^(((2(5[0-5]|[0-4]\\d))|[0-1]?\\d{1,2})(\\.((2(5[0-5]|[0-4]\\d))|[0-1]?\\d{1,2})){3}(:[0-9]{1,5})?(/.*)?)$".toRegex()
+  return regex1.matches(this) || regex2.matches(this)
+}
+
+fun String.isUrlOrHost() = this.isUrl() || this.isOnlyHost()
 fun String.isDeepLink() = this.startsWith("dweb://")
 
 /**
@@ -27,7 +31,7 @@ fun String.isDeepLink() = this.startsWith("dweb://")
  */
 fun String.toRequestUrl() = if (this.isUrl() || this.isDeepLink()) {
   this
-} else if (this.isHost()) {
+} else if (this.isOnlyHost()) {
   "https://$this"
 } else {
   null
