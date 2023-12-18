6e0bb5d48cdb2271da5eee692f5b6233069af7fb
waterbang <waterbang@qq.com>
2023-12-18 16:20:56 +0800

    :bug: [android] 修复代理启动过慢

--- next/kmp/DWebView/src/androidMain/kotlin/org/dweb_browser/dwebview/DWebView.android.kt
+++ next/kmp/DWebView/src/androidMain/kotlin/org/dweb_browser/dwebview/DWebView.android.kt
@@ -15,7 +15,7 @@ import androidx.webkit.WebSettingsCompat.FORCE_DARK_OFF
 import androidx.webkit.WebSettingsCompat.FORCE_DARK_ON
 import androidx.webkit.WebViewCompat
 import androidx.webkit.WebViewFeature
-import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.launch
 import org.dweb_browser.core.module.MicroModule
@@ -26,6 +26,7 @@ import org.dweb_browser.dwebview.engine.DWebViewEngine
 import org.dweb_browser.dwebview.proxy.DwebViewProxy
 import org.dweb_browser.dwebview.proxy.DwebViewProxyOverride
 import org.dweb_browser.helper.Bounds
+import org.dweb_browser.helper.ioAsyncExceptionHandler
 import org.dweb_browser.helper.withMainContext
 
 actual suspend fun IDWebView.Companion.create(
@@ -50,12 +51,6 @@ suspend fun IDWebView.Companion.create(
   activity: org.dweb_browser.helper.android.BaseActivity? = null
 ): IDWebView =
   withMainContext {
-    coroutineScope {
-      DwebViewProxy.prepare();
-      launch {
-        DwebViewProxyOverride.prepare()
-      }
-    }
     create(DWebViewEngine(context, remoteMM, options, activity), options.url)
   }
 
@@ -63,6 +58,17 @@ internal fun IDWebView.Companion.create(engine: DWebViewEngine, initUrl: String?
   DWebView(engine, initUrl)
 
 class DWebView(internal val engine: DWebViewEngine, initUrl: String? = null) : IDWebView(initUrl) {
+
+  companion object {
+    init {
+      GlobalScope.launch(ioAsyncExceptionHandler) {
+        DwebViewProxy.prepare();
+        launch {
+          DwebViewProxyOverride.prepare()
+        }
+      }
+    }
+  }
   init {
     engine.remoteMM.onAfterShutdown {
       destroy()
