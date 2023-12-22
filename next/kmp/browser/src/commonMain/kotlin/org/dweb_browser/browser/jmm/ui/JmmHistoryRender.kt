47f07c9262dd241edc3648e00d26441909426bd5
Gaubee <GaubeeBangeel@Gmail.com>
2023-12-22 11:34:21 +0800

    :package: [kmp/ios] 2.231221.1

--- next/kmp/browser/src/commonMain/kotlin/org/dweb_browser/browser/jmm/ui/JmmHistoryRender.kt
+++ next/kmp/browser/src/commonMain/kotlin/org/dweb_browser/browser/jmm/ui/JmmHistoryRender.kt
@@ -38,7 +38,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.scale
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
@@ -84,13 +83,13 @@ fun JmmHistoryController.ManagerViewRender(
 
     SingleChoiceSegmentedButtonRow(modifier = Modifier.fillMaxWidth()) {
       JmmTabs.entries.forEachIndexed { index, jmmTab ->
-         SegmentedButton(
-            selected = index == curTab.index,
-            onClick = { curTab = JmmTabs.entries[index] },
-            shape = RoundedCornerShape(16.dp),
-            icon = { Icon(imageVector = jmmTab.vector, contentDescription = jmmTab.title()) },
-            label = { Text(text = jmmTab.title()) }
-          )
+        SegmentedButton(
+          selected = index == curTab.index,
+          onClick = { curTab = JmmTabs.entries[index] },
+          shape = RoundedCornerShape(16.dp),
+          icon = { Icon(imageVector = jmmTab.vector, contentDescription = jmmTab.title()) },
+          label = { Text(text = jmmTab.title()) }
+        )
       }
     }
 
