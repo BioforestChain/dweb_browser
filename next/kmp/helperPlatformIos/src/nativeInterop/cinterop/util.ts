58b25093a98330577e7c448843edb4948ad399df
kingsword09 <kingsword09@gmail.com>
2024-01-12 17:41:47 +0800

    :bug: [ios] 修复ios脚本异常

--- next/kmp/helperPlatformIos/src/nativeInterop/cinterop/util.ts
+++ next/kmp/helperPlatformIos/src/nativeInterop/cinterop/util.ts
@@ -1,7 +1,7 @@
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 export const __dirname = path.dirname(fileURLToPath(import.meta.url));
-export const sourceCodeDir : string = __dirname + "/../../../../app/iOSApp/"
+export const sourceCodeDir : string = __dirname + "/../../../../app/iosApp/"
 
 export const runTasks = async (...actions: Array<() => Promise<number>>) => {
   for (const action of actions) {
