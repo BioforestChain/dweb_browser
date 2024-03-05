2a497385616e7558ee68c095ad96619e5d9696c9
pxh <575527452@qq.com>
2024-03-05 10:29:01 +0800

    :fire: js_common 移除了 view_model 中 Commonxxx 类

--- next/kmp/app/jsCommon/src/jsMain/kotlin/org/dweb_browser/js_common/view_model/CommonViewModel.kt
+++
@@ -1,4 +0,0 @@
-package org.dweb_browser.js_common.view_model
-
-typealias EncodeValueToString = (key: String, value: dynamic, syncType: SyncType) -> String
-typealias DecodeValueFromString = (key: String, value: String, syncType: SyncType) -> dynamic
