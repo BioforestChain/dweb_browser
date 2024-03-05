1e01b4e5324ef5fc6067d2e4656ed1414e541da0
pxh <575527452@qq.com>
2024-03-05 11:28:11 +0800

    :wrench: 更新 platformTest 工程配置，把 kotlin 目标同 local.properties 配置文件关联起来

--- next/kmp/platformTest/build.gradle.kts
+++ next/kmp/platformTest/build.gradle.kts
@@ -1,8 +1,34 @@
+import java.util.Properties
+
 plugins {
 //  alias(libs.plugins.kotlinMultiplatform)
 //  alias(libs.plugins.androidLibrary)
   id("kmp-library")
 }
+
+val localProperties = Properties()
+val localPropertiesFile = rootProject.file("local.properties")
+if (localPropertiesFile.exists()) {
+  localProperties.load(localPropertiesFile.inputStream())
+}
+
+val disabledApps = (localProperties.getProperty("app.disable") as String).split(",")
+  .map { it.trim().lowercase() }
+val enableAndroidApp = !disabledApps.contains("android")
+val enableIosApp = !disabledApps.contains("ios")
+val enableElectronApp = !disabledApps.contains("electron")
+val enableDesktop = !disabledApps.contains("desktop")
+val enableLibs = enableAndroidApp || enableIosApp || enableDesktop
+
+println("""
+  -----------------------
+  enableAndroidApp: $enableAndroidApp
+  enableIosApp: $enableIosApp
+  enableElectronApp: $enableElectronApp
+  enableDesktop: $enableDesktop
+  enableLibs: $enableLibs
+  -----------------------------------------------
+""".trimIndent())
 
 kotlin {
   kmpCommonTarget(project) {
@@ -10,8 +36,8 @@ kotlin {
       implementation(libs.test.kotlin.coroutines.test)
     }
   }
-  kmpAndroidTarget(project)
-  kmpIosTarget(project)
-//  kmpNodeJsTarget(project)
-  kmpDesktopTarget(project)
+  if(enableAndroidApp) kmpAndroidTarget(project)
+  if(enableIosApp)kmpIosTarget(project)
+  if(enableElectronApp) kmpNodeJsTarget(project)
+  if(enableDesktop) kmpDesktopTarget(project)
 }
