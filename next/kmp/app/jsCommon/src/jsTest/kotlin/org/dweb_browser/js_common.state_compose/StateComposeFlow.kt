995e8b186c3b83fb50d09b658f2f391061e52860
pxh <575527452@qq.com>
2024-03-05 18:48:38 +0800

    :white_check_mark: 新增 package org.dweb_browser.js_common.state_compose 的 StateComposeFlow 新功能对应的测试

--- next/kmp/app/jsCommon/src/jsTest/kotlin/org/dweb_browser/js_common.state_compose/StateComposeFlow.kt
+++ next/kmp/app/jsCommon/src/jsTest/kotlin/org/dweb_browser/js_common.state_compose/StateComposeFlow.kt
@@ -1,3 +1,4 @@
+import kotlinx.coroutines.CompletableDeferred
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
@@ -5,12 +6,13 @@ import kotlinx.coroutines.launch
 import org.dweb_browser.js_common.state_compose.ComposeFlow
 import org.dweb_browser.js_common.state_compose.state.EmitType
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 
 class TestStateComposeFlow(){
     @Test
     fun testPackagingCurrentStateOperationValueContainerString() {
-        val flow = ComposeFlow.createStateComposeFlowInstance<Int, String>()
+        val flow = ComposeFlow.createStateComposeFlowInstance<Int, Int, String>()
         val job = Job()
         CoroutineScope(Dispatchers.Default + job).launch{
             flow.collectServer{
@@ -25,8 +27,53 @@ class TestStateComposeFlow(){
             val value = flow.packagingCurrentStateOperationValueContainerString()
             console.log("value: ", value)
         }
-
-
     }
 
+    @Test
+    fun testEmitByClientWithAny(){
+        val flow = ComposeFlow.createStateComposeFlowInstance<Int, Int, String>()
+        val map = mapOf<String,ComposeFlow.StateComposeFlow<*, *, *>>(
+            "flow" to flow
+        )
+        var job: Job? = null
+        val targetValue  = 1
+        val deferred = CompletableDeferred<Any>()
+        CoroutineScope(Dispatchers.Default).launch {
+            job = map["flow"]?.collectClient{
+                deferred.complete(it)
+                job?.cancel()
+            }
+            job?.join()
+            assertEquals(deferred.await(), targetValue)
+            console.log("deferred.await(): ", deferred.await())
+        }
+
+        CoroutineScope(Dispatchers.Default).launch {
+            map["flow"]?.emitByClient(targetValue, emitType = EmitType.REPLACE)
+        }
+    }
+
+    @Test
+    fun testEmitByServerWithAny(){
+        val flow = ComposeFlow.createStateComposeFlowInstance<Int, Int, String>()
+        val map = mapOf<String,ComposeFlow.StateComposeFlow<*, *, *>>(
+            "flow" to flow
+        )
+        var job: Job? = null
+        val targetValue  = 1
+        val deferred = CompletableDeferred<Any>()
+        CoroutineScope(Dispatchers.Default).launch {
+            job = map["flow"]?.collectServer{
+                deferred.complete(it)
+                job?.cancel()
+            }
+            job?.join()
+            assertEquals(deferred.await(), targetValue)
+            console.log("deferred.await(): ", deferred.await())
+        }
+
+        CoroutineScope(Dispatchers.Default).launch {
+            map["flow"]?.emitByServer(targetValue, emitType = EmitType.REPLACE)
+        }
+    }
 }
--- next/kmp/app/jsCommon/src/jsTest/kotlin/org/dweb_browser/js_common/view_model/ViewModelSocket.kt
+++ next/kmp/app/jsCommon/src/jsTest/kotlin/org/dweb_browser/js_common/view_model/ViewModelSocket.kt
@@ -6,50 +6,23 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
-import org.dweb_browser.js_common.view_model.SyncData
-import org.dweb_browser.js_common.view_model.SyncType
 
 class TestSyncData{
     @Test
     fun testSerializedSyncData(){
-        val syncData = SyncData(
-            key = "key",
-            value = "value",
-            type = SyncType.REPLACE
-        )
 
-        val jsonString = Json.encodeToString(syncData)
-        console.log("jsonString: ", jsonString)
-        val d = Json.decodeFromString<SyncData>(jsonString)
-        console.log("d: ", d)
-        assertEquals("REPLACE", d.type.value)
     }
 }
 
 class TestSyncType{
     @Test
     fun testSerializedSyncType(){
-        val replace = SyncType.REPLACE
-        val jsonString = Json.encodeToString(replace)
-        console.log("jsonString", jsonString)
-        val replace2 = Json.decodeFromString<SyncType>(jsonString)
-        console.log("replace2: ", replace2)
-        assertEquals(replace.value, replace2.value)
+
     }
 
     @Test
     fun testEquals(){
-        val replace = SyncType.REPLACE
-        val jsonString = Json.encodeToString(replace)
-        val replace2 = Json.decodeFromString<SyncType>(jsonString)
-        val value = replace == replace2
-        console.log("replace == replace2", replace == replace2)
-        assertEquals(true, value)
 
-        when(replace){
-            replace2 -> console.log("相等")
-            else -> console.error("不相等")
-        }
     }
 }
 
