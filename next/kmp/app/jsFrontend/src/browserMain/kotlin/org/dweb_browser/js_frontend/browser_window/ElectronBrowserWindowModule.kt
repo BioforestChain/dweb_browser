ab2e1f4f7f167d85e926b04e607e4d8207d62424
pxh <575527452@qq.com>
2024-03-05 11:04:25 +0800

    :necktie: 更新了  package org.dweb_browser.js_frontend.browser_window 中的 ElectronBrowserWindowModule类的业务逻辑

--- next/kmp/app/jsFrontend/src/browserMain/kotlin/org/dweb_browser/js_frontend/browser_window/ElectronBrowserWindowModule.kt
+++ next/kmp/app/jsFrontend/src/browserMain/kotlin/org/dweb_browser/js_frontend/browser_window/ElectronBrowserWindowModule.kt
@@ -1,25 +1,18 @@
 package org.dweb_browser.js_frontend.browser_window
-
-import org.dweb_browser.js_frontend.view_model.ViewModel
-import org.dweb_browser.js_common.view_model.DecodeValueFromString
-import org.dweb_browser.js_common.view_model.EncodeValueToString
-import org.dweb_browser.js_frontend.view_model.ViewModelState
-
-interface IElectronBrowserWindowModule {
-    val moduleId: String
-    val controller: ElectronBrowserWindowController
-    val viewModel: ViewModel
-}
+import org.dweb_browser.js_common.view_model.DataState
+import org.dweb_browser.js_frontend.browser_window.ElectronBrowserWindowController
+import org.dweb_browser.js_frontend.view_model.OnSyncFromServer
+import org.dweb_browser.js_frontend.view_model.ViewModelDataState
 
 class ElectronBrowserWindowModule(
-    override val moduleId: String,/** example: demo.compose.app */
-    encodeValueToString: EncodeValueToString,
-    decodeValueFromString: DecodeValueFromString,
-) : IElectronBrowserWindowModule {
-    override val controller: ElectronBrowserWindowController = ElectronBrowserWindowController()
-    override val viewModel: ViewModel = ViewModel(
-        ViewModelState(mutableMapOf()),
-        encodeValueToString = encodeValueToString,
-        decodeValueFromString = decodeValueFromString
+    val VMId: String,
+    dataState: DataState,
+    onSyncFromServer: OnSyncFromServer
+) {
+    val viewModelDataState =  ViewModelDataState(
+        dataState = dataState,
+        VMId = VMId,
+        onSyncFromServer = onSyncFromServer
     )
+    val controller: ElectronBrowserWindowController = ElectronBrowserWindowController()
 }
