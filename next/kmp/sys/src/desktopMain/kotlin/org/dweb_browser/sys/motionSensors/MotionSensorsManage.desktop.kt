20d9103b91721bf487adb4e2230c2e488b5d7cce
Gaubee <GaubeeBangeel@Gmail.com>
2024-03-07 20:28:01 +0800

    :sparkles: [kmp/sys] 实现了MotionSensorsManage的desktop初步适配
    桌面端默认没有运动传感器

---
+++ next/kmp/sys/src/desktopMain/kotlin/org/dweb_browser/sys/motionSensors/MotionSensorsManage.desktop.kt
@@ -0,0 +1,19 @@
+package org.dweb_browser.sys.motionSensors
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emptyFlow
+import org.dweb_browser.core.module.NativeMicroModule
+
+actual class MotionSensorsManage actual constructor(mm: NativeMicroModule) {
+  actual val isSupportAccelerometer = false
+
+  actual fun getAccelerometerFlow(fps: Double?): Flow<Axis> {
+    return emptyFlow()
+  }
+
+  actual val isSupportGyroscope = false
+
+  actual fun getGyroscopeFlow(fps: Double?): Flow<Axis> {
+    return emptyFlow()
+  }
+}
