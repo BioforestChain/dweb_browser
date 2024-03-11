3f9fbd3ce426f9ed4875c78354c5399783487e7e
kingsword09 <kingsword09@gmail.com>
2024-03-11 09:03:49 +0800

    :fire: [kmp/desktopApp] 移除jcef

--- next/kmp/app/desktopApp/build.gradle.kts
+++ next/kmp/app/desktopApp/build.gradle.kts
@@ -15,43 +15,18 @@ kotlin {
   kmpDesktopTarget(project) {
     dependencies {
       implementation(projects.platformDesktop)
-      implementation(libs.jcef.maven)
     }
   }
 }
 
 compose.desktop {
   application {
-//    mainClass = "MainKt"
-    mainClass = "StartKt"
-
-    args += listOf(
-      "--add-opens java.desktop/sun.awt=ALL-UNNAMED",
-      "--add-opens java.desktop/sun.lwawt=ALL-UNNAMED",
-      "--add-opens java.desktop/sun.lwawt.macosx=ALL-UNNAMED"
-    )
-
-    jvmArgs += listOf(
-      "--add-opens java.desktop/sun.awt=ALL-UNNAMED",
-      "--add-opens java.desktop/sun.lwawt=ALL-UNNAMED",
-      "--add-opens java.desktop/sun.lwawt.macosx=ALL-UNNAMED",
-    )
+    mainClass = "MainKt"
 
     nativeDistributions {
       targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
       packageName = "info.bagen.dwebbrowser"
       packageVersion = "1.0.0"
     }
-  }
-}
-
-afterEvaluate {
-  tasks.withType<JavaExec> {
-    jvmArgs("--add-opens", "java.desktop/sun.awt=ALL-UNNAMED")
-    jvmArgs("--add-opens", "java.desktop/sun.lwawt=ALL-UNNAMED")
-    jvmArgs("--add-opens", "java.desktop/sun.lwawt.macosx=ALL-UNNAMED")
-    jvmArgs("--add-exports", "java.base/java.lang=ALL-UNNAMED")
-    jvmArgs("--add-exports", "java.desktop/sun.awt=ALL-UNNAMED")
-    jvmArgs("--add-exports", "java.desktop/sun.java2d=ALL-UNNAMED")
   }
 }
