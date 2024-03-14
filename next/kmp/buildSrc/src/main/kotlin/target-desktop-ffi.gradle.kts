854a48fb72bd005b6646f746c576417fe7c8d85b
kingsword09 <kingsword09@gmail.com>
2024-03-14 18:16:36 +0800

    :sparkles: [kmp/buildSrc] 添加target-desktop-ffi

---
+++ next/kmp/buildSrc/src/main/kotlin/target-desktop-ffi.gradle.kts
@@ -0,0 +1,26 @@
+import org.gradle.accessors.dm.LibrariesForLibs
+import org.gradle.kotlin.dsl.the
+
+plugins {
+  id("target-common")
+}
+
+
+val libs = the<LibrariesForLibs>()
+
+kotlin {
+  kmpCommonTarget(project) {
+    dependencies {
+      implementation(libs.squareup.okio)
+      implementation(libs.kotlinx.datetime)
+    }
+  }
+  kmpDesktopTarget(project) {
+    dependencies {
+      api(libs.java.jna)
+    }
+  }
+  sourceSets.all {
+    languageSettings.optIn("kotlinx.cinterop.ExperimentalForeignApi")
+  }
+}
