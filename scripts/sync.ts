79e367a5e7f5fa83fdf21807cf3abfdba8ab8568
kingsword09 <kingsword09@gmail.com>
2024-03-25 10:38:58 +0800

    :fire: [scripts] 移除 C# 资源同步任务

--- scripts/sync.ts
+++ scripts/sync.ts
@@ -2,13 +2,11 @@ import { SyncTask } from "./helper/SyncTask.ts";
 // import { androidSyncTask } from "./sync-android.ts";
 import { kmpSyncTask } from "./sync-kmp.ts";
 // import { syncServerTask } from "./sync-desktop.ts";
-import { nextSyncTask } from "./sync-next.ts";
 
 export const syncTask = SyncTask.concat(
   // syncServerTask,
   kmpSyncTask,
   // androidSyncTask,
-  nextSyncTask
 );
 if (import.meta.main) {
   syncTask.auto();
