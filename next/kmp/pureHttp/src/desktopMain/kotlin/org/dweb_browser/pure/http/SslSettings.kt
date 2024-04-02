db44179f9e8b09e3d0a81a42884ed2e19adb5060
kingsword09 <kingsword09@gmail.com>
2024-04-02 12:27:29 +0800

    :bug: [kmp/desktopApp] 修复JVM应用信任证书异常

--- next/kmp/pureHttp/src/desktopMain/kotlin/org/dweb_browser/pure/http/SslSettings.kt
+++ next/kmp/pureHttp/src/desktopMain/kotlin/org/dweb_browser/pure/http/SslSettings.kt
@@ -4,8 +4,10 @@ import io.ktor.network.tls.certificates.buildKeyStore
 import io.ktor.network.tls.certificates.saveToFile
 import org.dweb_browser.helper.randomUUID
 import java.io.File
+import java.security.KeyStore
+import java.security.cert.CertificateException
+import java.security.cert.X509Certificate
 import javax.net.ssl.SSLContext
-import javax.net.ssl.TrustManager
 import javax.net.ssl.TrustManagerFactory
 import javax.net.ssl.X509TrustManager
 
@@ -25,6 +27,14 @@ object SslSettings {
     it.saveToFile(keyStoreFile, keyStorePassword)
   }
 
+  // JVM默认信任证书
+  val defaultTrustManager: X509TrustManager by lazy {
+    val trustManagerFactory =
+      TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm())
+    trustManagerFactory.init(null as KeyStore?)
+    trustManagerFactory.trustManagers.first { it is X509TrustManager } as X509TrustManager
+  }
+
   fun getTrustManagerFactory(): TrustManagerFactory? {
     val trustManagerFactory =
       TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm())
@@ -32,15 +42,38 @@ object SslSettings {
     return trustManagerFactory
   }
 
+  // 必须添加JVM默认信任证书和自签证书，否则会出现 httpStatusCode: 417 Expectation Failed
+  @Suppress("CustomX509TrustManager")
+  private class CompositeX509TrustManager(
+    val primaryTrustManager: X509TrustManager?,
+    val secondaryTrustManager: X509TrustManager?
+  ) : X509TrustManager {
+
+    override fun checkClientTrusted(chain: Array<out X509Certificate>, authType: String) {
+      try {
+        primaryTrustManager?.checkClientTrusted(chain, authType)
+      } catch (ex: CertificateException) {
+        secondaryTrustManager?.checkClientTrusted(chain, authType)
+      }
+    }
+
+    override fun checkServerTrusted(chain: Array<out X509Certificate>, authType: String) {
+      try {
+        primaryTrustManager?.checkServerTrusted(chain, authType)
+      } catch (ex: CertificateException) {
+        secondaryTrustManager?.checkServerTrusted(chain, authType)
+      }
+    }
+
+    override fun getAcceptedIssuers(): Array<X509Certificate> =
+      (primaryTrustManager?.acceptedIssuers.orEmpty()
+        .toList() + secondaryTrustManager?.acceptedIssuers.orEmpty().toList()).toTypedArray()
+  }
+
   fun getSslContext(): SSLContext {
     val sslContext = SSLContext.getInstance("TLS")
-    sslContext.init(null, mutableListOf<TrustManager>().run {
-      getTrustManagerFactory()?.trustManagers?.also {
-        println("trustManagers=${it.joinToString(", ")}")
-        addAll(it)
-      }
-      toTypedArray()
-    }, null)
+    val trustManager = CompositeX509TrustManager(defaultTrustManager, trustManager)
+    sslContext.init(null, arrayOf(trustManager), null)
     return sslContext
   }
 
