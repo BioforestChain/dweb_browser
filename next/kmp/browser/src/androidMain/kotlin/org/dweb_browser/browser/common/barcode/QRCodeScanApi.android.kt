5033e4dddff5ce00bf92fb0cb3fa0d0986ddc66a
jackie-yellow <hl19081555@gmail.com>
2024-04-02 15:37:15 +0800

    :bug: [kmp/browser] 修复扫码后，弹出网页下载界面。优化扫码结果的处理方式。
    扫码结果如下：
    1. 如果是deeplink，直接打开
    2. 如果不是deeplink，就判断是否是正常地址，如果是weburl，打开browser并加载地址
    3. 如果既不是deeplink也不是weburl，那么就作为关键字，打开browser的搜索界面

--- next/kmp/browser/src/androidMain/kotlin/org/dweb_browser/browser/common/barcode/QRCodeScanApi.android.kt
+++ next/kmp/browser/src/androidMain/kotlin/org/dweb_browser/browser/common/barcode/QRCodeScanApi.android.kt
@@ -2,45 +2,18 @@ package org.dweb_browser.browser.common.barcode
 
 import android.content.Context
 import android.content.Intent
-import android.graphics.BitmapFactory
 import android.media.AudioManager
 import android.net.Uri
-import android.util.Log
-import android.widget.Toast
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.rememberLauncherForActivityResult
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.camera.core.Camera
-import androidx.camera.core.CameraSelector
-import androidx.camera.core.ImageAnalysis
-import androidx.camera.core.ImageCapture
-import androidx.camera.core.Preview
-import androidx.camera.lifecycle.ProcessCameraProvider
-import androidx.camera.view.PreviewView
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.asAndroidBitmap
 import androidx.compose.ui.graphics.asImageBitmap
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.viewinterop.AndroidView
-import androidx.core.content.ContextCompat
 import com.google.mlkit.vision.barcode.BarcodeScannerOptions
 import com.google.mlkit.vision.barcode.BarcodeScanning
 import com.google.mlkit.vision.barcode.common.Barcode
 import com.google.mlkit.vision.common.InputImage
-import org.dweb_browser.browser.BrowserI18nResource
 import org.dweb_browser.browser.util.regexDeepLink
 import org.dweb_browser.core.module.getAppContext
 import org.dweb_browser.helper.isWebUrl
-import java.util.concurrent.Executors
 import kotlin.math.abs
 
 actual fun beepAudio() {
@@ -98,24 +71,34 @@ actual fun transformPoint(
 
 actual fun openDeepLink(data: String, showBackground: Boolean): Boolean {
   val context = getAppContext()
-  val deepLink = if (data.isWebUrl()) {
-    "dweb://openinbrowser?url=$data"
-  } else data
-
-  deepLink.regexDeepLink()?.let { dwebLink ->
-    context.startActivity(Intent().apply {
-      `package` = context.packageName
-      action = Intent.ACTION_VIEW
-      this.data = Uri.parse(dwebLink)
-      addCategory("android.intent.category.BROWSABLE")
-      addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-      putExtra("showBackground", showBackground)
-    })
-    return true
-  } ?: run {
-    Toast.makeText(
-      context, BrowserI18nResource.QRCode.toast_mismatching.text.format(data), Toast.LENGTH_SHORT
-    ).show()
-    return false
+  // 下盘的是否是 DeepLink，如果不是的话，判断是否是
+  val deepLink = data.regexDeepLink() ?: run {
+    if (data.isWebUrl()) { "dweb://openinbrowser?url=$data" } else "dweb://search?q=$data"
   }
+
+  context.startActivity(Intent().apply {
+    `package` = context.packageName
+    action = Intent.ACTION_VIEW
+    this.data = Uri.parse(deepLink)
+    addCategory("android.intent.category.BROWSABLE")
+    addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+    putExtra("showBackground", showBackground)
+  })
+  return true
+//  deepLink.regexDeepLink()?.let { dwebLink ->
+//    context.startActivity(Intent().apply {
+//      `package` = context.packageName
+//      action = Intent.ACTION_VIEW
+//      this.data = Uri.parse(dwebLink)
+//      addCategory("android.intent.category.BROWSABLE")
+//      addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+//      putExtra("showBackground", showBackground)
+//    })
+//    return true
+//  } ?: run {
+//    Toast.makeText(
+//      context, BrowserI18nResource.QRCode.toast_mismatching.text.format(data), Toast.LENGTH_SHORT
+//    ).show()
+//    return false
+//  }
 }
