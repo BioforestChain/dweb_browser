c9f5f5cd57e6faa9f3032179ca4364c3081eae97
kingsword09 <kingsword09@gmail.com>
2024-04-08 18:27:19 +0800

    :bug: [kmp/desktopApp] 修复Storage权限申请

--- next/kmp/sys/src/desktopMain/kotlin/org/dweb_browser/sys/filechooser/FileChooserManage.desktop.kt
+++ next/kmp/sys/src/desktopMain/kotlin/org/dweb_browser/sys/filechooser/FileChooserManage.desktop.kt
@@ -1,12 +1,23 @@
 package org.dweb_browser.sys.filechooser
 
 import org.dweb_browser.core.module.MicroModule
+import org.dweb_browser.core.std.permission.AuthorizationStatus
 import org.dweb_browser.sys.ext.awaitComposeWindow
+import org.dweb_browser.sys.permission.SystemPermissionAdapterManager
+import org.dweb_browser.sys.permission.SystemPermissionName
 import java.io.File
 import javax.swing.JFileChooser
 import javax.swing.filechooser.FileFilter
 
 actual class FileChooserManage actual constructor() {
+  init {
+    SystemPermissionAdapterManager.append {
+      if(task.name == SystemPermissionName.STORAGE) {
+        AuthorizationStatus.GRANTED
+      } else null
+    }
+  }
+  
   actual suspend fun openFileChooser(
     microModule: MicroModule,
     accept: String,
