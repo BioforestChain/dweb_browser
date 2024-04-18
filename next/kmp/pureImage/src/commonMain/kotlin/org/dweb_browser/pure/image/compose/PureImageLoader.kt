0dcb7ce76d21700d41e6d4accdf76439ba6deb66
kingsword09 <kingsword09@gmail.com>
2024-04-18 10:08:41 +0800

    :bug: [kmp] 桌面端PureImageLoader改为使用web渲染，coil存在异常

--- next/kmp/pureImage/src/commonMain/kotlin/org/dweb_browser/pure/image/compose/PureImageLoader.kt
+++ next/kmp/pureImage/src/commonMain/kotlin/org/dweb_browser/pure/image/compose/PureImageLoader.kt
@@ -4,38 +4,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.unit.Dp
 import org.dweb_browser.pure.image.offscreenwebcanvas.FetchHook
 
+@Composable
+expect fun PureImageLoader.Companion.SmartLoad(
+  url: String, maxWidth: Dp, maxHeight: Dp, hook: FetchHook? = null
+): ImageLoadResult
+
 interface PureImageLoader {
   @Composable
   fun Load(
     url: String, maxWidth: Dp, maxHeight: Dp, hook: FetchHook?
   ): ImageLoadResult
 
-  companion object {
-    @Composable
-    fun SmartLoad(
-      url: String, maxWidth: Dp, maxHeight: Dp, hook: FetchHook? = null
-    ): ImageLoadResult {
-      var fixUrl = url
-      if (fixUrl.startsWith("data://localhost/")) {
-        fixUrl = fixUrl.replace("data://localhost/", "data:")
-      }
-      val result = LocalCoilImageLoader.current.Load(fixUrl, maxWidth, maxHeight, hook)
-      if (result.isError) {
-        return LocalWebImageLoader.current.Load(fixUrl, maxWidth, maxHeight, hook)
-      }
-      return result
-    }
-
-    @Composable
-    fun WebLoad(
-      url: String, maxWidth: Dp, maxHeight: Dp, hook: FetchHook? = null
-    ): ImageLoadResult {
-      var fixUrl = url
-      if (fixUrl.startsWith("data://localhost/")) {
-        fixUrl = fixUrl.replace("data://localhost/", "data:")
-      }
-
-      return LocalWebImageLoader.current.Load(fixUrl, maxWidth, maxHeight, hook)
-    }
-  }
+  companion object
 }
