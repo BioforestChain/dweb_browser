0258b190612e529fd4e6df6b0df40a4fcb8825ab
kingsword09 <kingsword09@gmail.com>
2024-04-26 14:35:36 +0800

    :bug: [kmp/desktopApp] 修复desktopApp无法播放视频异常

--- next/kmp/platformDesktop/src/desktopMain/kotlin/org/dweb_browser/platform/desktop/webview/jxbrowser.kt
+++ next/kmp/platformDesktop/src/desktopMain/kotlin/org/dweb_browser/platform/desktop/webview/jxbrowser.kt
@@ -3,6 +3,7 @@ package org.dweb_browser.platform.desktop.webview
 import com.teamdev.jxbrowser.VersionInfo
 import com.teamdev.jxbrowser.engine.Engine
 import com.teamdev.jxbrowser.engine.EngineOptions
+import com.teamdev.jxbrowser.engine.ProprietaryFeature
 import com.teamdev.jxbrowser.engine.RenderingMode.HARDWARE_ACCELERATED
 import com.teamdev.jxbrowser.engine.RenderingMode.OFF_SCREEN
 
@@ -15,6 +16,10 @@ object WebviewEngine {
   fun hardwareAccelerated(optionsBuilder: (EngineOptions.Builder.() -> Unit)? = null): Engine =
     Engine.newInstance(EngineOptions.newBuilder(HARDWARE_ACCELERATED).run {
       optionsBuilder?.invoke(this)
+      enableProprietaryFeature(ProprietaryFeature.HEVC)
+      enableProprietaryFeature(ProprietaryFeature.WIDEVINE)
+      enableProprietaryFeature(ProprietaryFeature.AAC)
+      enableProprietaryFeature(ProprietaryFeature.H_264)
       this.licenseKey(licenseKey)
       build()
     })
