380215978ada7886500e003f7474d4280598baf3
waterbang <409071692@qq.com>
2024-05-21 17:38:31 +0800

    :bug: [desktop] 修复第一次打开白屏

--- next/kmp/browser/src/commonMain/kotlin/org/dweb_browser/browser/mwebview/MultiWebViewRender.kt
+++ next/kmp/browser/src/commonMain/kotlin/org/dweb_browser/browser/mwebview/MultiWebViewRender.kt
@@ -16,7 +16,6 @@ import kotlinx.coroutines.launch
 import org.dweb_browser.dwebview.Render
 import org.dweb_browser.dwebview.rememberCanGoBack
 import org.dweb_browser.dwebview.rememberCanGoForward
-import org.dweb_browser.helper.platform.LocalPureViewController
 import org.dweb_browser.sys.window.render.LocalWindowController
 import org.dweb_browser.sys.window.render.watchedIsMaximized
 
@@ -28,46 +27,27 @@ fun MultiWebViewController.Render(
   height: Float,
 ) {
   val win = LocalWindowController.current
-  val pureViewController = LocalPureViewController.current
+  var list by remember {
+    mutableStateOf(listOf<MultiWebViewController.MultiViewItem>())
+  }
+  DisposableEffect(webViewList) {
+    val off = webViewList.onChange {
+      list = it.toList()
+      if (list.isEmpty()) {
+        win.tryCloseOrHide()
+      }
+      return@onChange;
+    }
+    list = webViewList.toList()
+    onDispose {
+      off()
+    }
+  }
+
   Box(modifier) {
-    var list by remember {
-      mutableStateOf(webViewList.toList())
-    }
-    DisposableEffect(webViewList) {
-      val off = webViewList.onChange {
-        list = it.toList()
-        if (list.isEmpty()) {
-          win.tryCloseOrHide()
-        }
-        return@onChange;
-      }
-      onDispose {
-        off()
-      }
-    }
     list.forEach { viewItem ->
       key(viewItem.webviewId) {
-        val webView = viewItem.webView
-
-        /// 返回按钮的拦截只跟最后一个视图有关系，直到这最后一个视图被关闭了
-        if (viewItem == list.last()) {
-          val isMaximized by win.watchedIsMaximized()
-          // 在 MWebView 的全屏窗口中，默认将返回按钮的行为与应用退出关联在一起
-          if (win.state.canGoForward != null) {
-            win.state.canGoForward = webView.rememberCanGoForward()
-          }
-          val canGoBack =
-            if (isMaximized) true else webView.closeWatcher.canClose || webView.rememberCanGoBack()
-          win.GoBackHandler(canGoBack) {
-            if (webView.canGoBack()) {
-              webView.goBack()
-            } else if (list.size > 1) {
-              viewItem.coroutineScope.launch {
-                closeWebView(viewItem.webviewId)
-              }
-            }
-          }
-        }
+        LatestEffect(viewItem, list)
 
         /// 为了防止在窗口状态下，webview返回时失真问题。所以在webview加载完成后出发刷新
         val density = LocalDensity.current.density
@@ -81,6 +61,33 @@ fun MultiWebViewController.Render(
           // 开始分平台渲染web view
           viewItem.webView.Render(Modifier.fillMaxSize())
           AfterViewItemRender(viewItem)
+        }
+      }
+    }
+  }
+}
+
+@Composable
+private fun MultiWebViewController.LatestEffect(
+  viewItem: MultiWebViewController.MultiViewItem,
+  list: List<MultiWebViewController.MultiViewItem>
+) {
+  val webView = viewItem.webView
+  /// 返回按钮的拦截只跟最后一个视图有关系，直到这最后一个视图被关闭了
+  if (viewItem == list.last()) {
+    val isMaximized by win.watchedIsMaximized()
+    // 在 MWebView 的全屏窗口中，默认将返回按钮的行为与应用退出关联在一起
+    if (win.state.canGoForward != null) {
+      win.state.canGoForward = webView.rememberCanGoForward()
+    }
+    val canGoBack =
+      if (isMaximized) true else webView.closeWatcher.canClose || webView.rememberCanGoBack()
+    win.GoBackHandler(canGoBack) {
+      if (webView.canGoBack()) {
+        webView.goBack()
+      } else if (list.size > 1) {
+        viewItem.coroutineScope.launch {
+          closeWebView(viewItem.webviewId)
         }
       }
     }
