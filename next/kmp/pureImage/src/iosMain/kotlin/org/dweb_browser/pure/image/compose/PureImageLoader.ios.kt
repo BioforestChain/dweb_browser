5fd7d83ec179a5cc98c361e135407161073fb0fa
Gaubee <GaubeeBangeel@Gmail.com>
2024-05-29 08:22:03 +0800

    :children_crossing: [kmp/pureImage] 改进了ios的图片加载策略

--- next/kmp/pureImage/src/iosMain/kotlin/org/dweb_browser/pure/image/compose/PureImageLoader.ios.kt
+++ next/kmp/pureImage/src/iosMain/kotlin/org/dweb_browser/pure/image/compose/PureImageLoader.ios.kt
@@ -6,11 +6,15 @@ import org.dweb_browser.pure.image.offscreenwebcanvas.FetchHook
 
 @Composable
 actual fun PureImageLoader.Companion.SmartLoad(
-  url: String, maxWidth: Dp, maxHeight: Dp, hook: FetchHook?
+  url: String, maxWidth: Dp, maxHeight: Dp, hook: FetchHook?,
 ): ImageLoadResult {
   var fixUrl = url
   if (fixUrl.startsWith("data://localhost/")) {
     fixUrl = fixUrl.replace("data://localhost/", "data:")
+  }
+  // svg 尝试优先使用 WebImageLoader，但不强制
+  if (fixUrl.endsWith(".svg")) {
+    return LocalWebImageLoader.current.Load(fixUrl, maxWidth, maxHeight, hook)
   }
   val result = LocalCoilImageLoader.current.Load(fixUrl, maxWidth, maxHeight, hook)
   if (result.isError) {
