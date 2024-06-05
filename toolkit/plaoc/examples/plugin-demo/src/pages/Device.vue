c6f089914cc7d7bf724fad14f72883c89631b27c
waterbang <409071692@qq.com>
2024-06-05 14:30:15 +0800

    :construction: [assets/examples] 临时添加获取联系人测试
    ios 不支持，需要添加垫片

--- toolkit/plaoc/examples/plugin-demo/src/pages/Device.vue
+++ toolkit/plaoc/examples/plugin-demo/src/pages/Device.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
-import { onMounted, ref } from "vue";
-import LogPanel, { toConsole, } from "../components/LogPanel.vue";
-import type { HTMLDeviceElement } from "@plaoc/plugins";
 import { isDweb } from "@plaoc/is-dweb";
-
+import type { HTMLDeviceElement } from "@plaoc/plugins";
+import { onMounted, ref } from "vue";
+import LogPanel, { toConsole } from "../components/LogPanel.vue";
 
 const title = "device";
 const $deviceElement = ref<HTMLDeviceElement>();
@@ -23,9 +22,37 @@ async function getUUID() {
 }
 
 function isDwebBrowser() {
-  console.log("isDwebBrowser=>",isDweb())
+  console.log("isDwebBrowser=>", isDweb());
 }
 
+const props = ["name", "email", "tel", "address", "icon"];
+const opts = { multiple: true };
+
+declare global {
+  interface Navigator {
+    contacts?: Contacts; // 假设contacts是一个可选属性，并且它的类型是Contacts
+  }
+  interface Contacts {
+    // 定义你期望的Contacts对象属性和方法
+    select: (props: string[], opts: { multiple: boolean }) => Promise<Contact[]>;
+  }
+  interface Contact {
+    name: string;
+    email: string;
+    tel: string;
+    address: string;
+    icon: string;
+  }
+}
+async function getContacts() {
+  try {
+    const contacts = await navigator?.contacts?.select(props, opts);
+    console.log(contacts);
+  } catch (ex) {
+    // Handle any errors here.
+    console.log(ex);
+  }
+}
 </script>
 <template>
   <dweb-device ref="$deviceElement"></dweb-device>
@@ -40,6 +67,10 @@ function isDwebBrowser() {
     <article class="card-body">
       <h2 class="card-title">是否是dweb</h2>
       <v-btn color="indigo-darken-3" @click="isDwebBrowser">是否在dweb环境下</v-btn>
+    </article>
+    <article class="card-body">
+      <h2 class="card-title">获取联系人(android/ios suport)</h2>
+      <v-btn color="indigo-darken-3" @click="getContacts">获取联系人</v-btn>
     </article>
   </div>
   <div class="divider">LOG</div>
