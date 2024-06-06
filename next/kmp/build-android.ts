705cd893b1edfd7f52082fdcb0000a20aa131a3e
Gaubee <GaubeeBangeel@Gmail.com>
2024-06-06 14:15:01 +0800

    :wrench: [kmp] 改进build-android.ts脚本，更好的版本号更新逻辑

--- next/kmp/build-android.ts
+++ next/kmp/build-android.ts
@@ -1,123 +1,132 @@
+import fs from "node:fs";
 import { WalkFiles } from "../../scripts/helper/WalkDir.ts";
 import { createBaseResolveTo } from "../../scripts/helper/resolveTo.ts";
 const resolveTo = createBaseResolveTo(import.meta.url);
 
-// 发布版本的时候，升级下版本信息 versionCode和versionName
-upgradeVersionInfo(resolveTo("gradle/libs.versions.toml"))
-
 // 运行 assembleRelease 命令，继承输出
 const cmd = Deno.build.os === "windows" ? resolveTo("gradlew.bat") : resolveTo("gradlew");
-const args = ["assembleRelease", "assembleDebug", "bundleRelease"];
-console.log(">", cmd, ...args);
+const args = ["assembleRelease", "bundleRelease"];
+const doBundle = async () => {
+  console.log(">", cmd, ...args);
 
-// 执行 assembleRelease
-const gradle = new Deno.Command(cmd, {
-  cwd: resolveTo(),
-  args: args,
-  stdout: "inherit",
-  stderr: "inherit",
-});
+  // 执行 assembleRelease
+  const gradle = new Deno.Command(cmd, {
+    cwd: resolveTo(),
+    args: args,
+    stdout: "inherit",
+    stderr: "inherit",
+  });
 
-// 等待命令完成
-const { code } = await gradle.spawn().status;
-if (code !== 0) {
-  console.error(`${cmd} 失败`);
-  Deno.exit(code);
-}
+  // 等待命令完成
+  const { code } = await gradle.spawn().status;
+  if (code !== 0) {
+    console.error(`${cmd} 失败`);
+    Deno.exit(code);
+  }
+};
 
 // 定义APK目标目录
 const OUTPUT_DIR = resolveTo("./app/androidApp/release");
-const BUILD_OUTPUT_DIR = resolveTo("./app/androidApp/build/outputs/apk");
-const CONDITIONS = ["debug", "release", "with"];
 
 // // 创建、清空目标目录
 // Deno.removeSync(OUTPUT_DIR, { recursive: true });
 // Deno.mkdirSync(OUTPUT_DIR, { recursive: true });
 
-// 遍历构建输出目录，筛选需要的文件夹
-for await (const dirEntry of Deno.readDir(BUILD_OUTPUT_DIR)) {
-  if (dirEntry.isDirectory) {
-    const variant = dirEntry.name;
-    if (CONDITIONS.some((cond) => variant.startsWith(cond))) {
-      console.log(`Processing ${variant}...`);
-      const variantPath = `${BUILD_OUTPUT_DIR}/${variant}`;
+const doCopy = async () => {
+  const CONDITIONS = ["debug", "release", "with"];
+  const copyFile = async (baseDir: string, dirEntry: Deno.DirEntry) => {
+    if (dirEntry.isDirectory) {
+      const variant = dirEntry.name;
+      if (CONDITIONS.some((cond) => variant.startsWith(cond))) {
+        console.log(`Processing ${variant}...`);
+        const variantPath = `${baseDir}/${variant}`;
 
-      for await (const fileEntry of WalkFiles(variantPath)) {
-        if (fileEntry.entryname.endsWith(".apk")) {
-          const version = fileEntry.entryname.match(/v\d+\.\d+\.\d+/);
-          console.log(`  Copying ${fileEntry.relativepath}`);
-          const outputDir = `${OUTPUT_DIR}/${version}`;
-          // 创建、清空目标目录
-          Deno.mkdirSync(outputDir, { recursive: true });
-          Deno.mkdirSync(outputDir, { recursive: true });
+        for await (const fileEntry of WalkFiles(variantPath)) {
+          if (fileEntry.entryname.endsWith(".apk") || fileEntry.entryname.endsWith(".aab")) {
+            const version = fileEntry.entryname.match(/v\d+\.\d+\.\d+/);
+            console.log(`  Copying ${fileEntry.relativepath}`);
+            const outputDir = `${OUTPUT_DIR}/android_${version}`;
+            // 创建目标目录
+            Deno.mkdirSync(outputDir, { recursive: true });
 
-          await Deno.copyFile(fileEntry.entrypath, `${outputDir}/${fileEntry.entryname}`);
+            await Deno.copyFile(fileEntry.entrypath, `${outputDir}/${fileEntry.entryname}`);
+          }
         }
       }
     }
+  };
+  // 遍历构建输出目录，筛选需要的文件夹
+  const BUILD_OUTPUT_DIR = resolveTo("./app/androidApp/build/outputs/apk");
+  for await (const dirEntry of Deno.readDir(BUILD_OUTPUT_DIR)) {
+    await copyFile(BUILD_OUTPUT_DIR, dirEntry);
   }
-}
 
-// 遍历构建输出目录，筛选需要的文件夹(针对aab文件)
-const BUILD_OUTPUT_DIR_BUNDLE = resolveTo("./app/androidApp/build/outputs/bundle");
-for await (const dirEntry of Deno.readDir(BUILD_OUTPUT_DIR_BUNDLE)) {
-  if (dirEntry.isDirectory) {
-    const variant = dirEntry.name;
-    if (CONDITIONS.some((cond) => variant.startsWith(cond))) {
-      console.log(`Processing ${variant}...`);
-      const variantPath = `${BUILD_OUTPUT_DIR_BUNDLE}/${variant}`;
-
-      for await (const fileEntry of WalkFiles(variantPath)) {
-        if (fileEntry.entryname.endsWith(".aab")) {
-          const version = fileEntry.entryname.match(/v\d+\.\d+\.\d+/);
-          console.log(`  Copying ${fileEntry.relativepath}`);
-          const outputDir = `${OUTPUT_DIR}/android_${version}`;
-          // 创建、清空目标目录
-          Deno.mkdirSync(outputDir, { recursive: true });
-          Deno.mkdirSync(outputDir, { recursive: true });
-
-          await Deno.copyFile(fileEntry.entrypath, `${outputDir}/${fileEntry.entryname}`);
-        }
-      }
-    }
+  // 遍历构建输出目录，筛选需要的文件夹(针对aab文件)
+  const BUILD_OUTPUT_DIR_BUNDLE = resolveTo("./app/androidApp/build/outputs/bundle");
+  for await (const dirEntry of Deno.readDir(BUILD_OUTPUT_DIR_BUNDLE)) {
+    await copyFile(BUILD_OUTPUT_DIR_BUNDLE, dirEntry);
   }
-}
+};
 
 console.log(`All required APKs have been copied to ${OUTPUT_DIR}`);
 
 // 异步读取并升级版本信息
-async function upgradeVersionInfo(filePath: string) {
+const upgradeVersionInfo = async (filePath: string) => {
   try {
     // 读取文件内容
-    const content = await Deno.readTextFile(filePath);
-    
-    // 将文件内容按行分割
-    const lines = content.split("\r\n");
+    const content = fs.readFileSync(filePath, "utf-8");
 
-    // 遍历每一行，判断是否是 versionCode和versionName，然后进行修改
-    for(let index = 0; index < lines.length; index++) {
-      const line = lines[index]
-      if(line.startsWith("versionCode")) { // 修改 versionCode
-        const versionCode = line.substring(line.indexOf("=") + 1).replaceAll("\"", "").trim()
-        lines[index] = `versionCode = "${parseInt(versionCode) + 1}"`
-        console.log(`versionCode ${versionCode} >> ${lines[index]}`)
-      } else if(line.startsWith("versionName")) { // 修改 versionName
-        const now = new Date()
-        const currentDate = `${now.getFullYear()-2000}${(now.getMonth()+1).toString().padStart(2, '0')}${(now.getDate()).toString().padStart(2, '0')}`
-        const versionName = line.substring(line.indexOf("=") + 1).replaceAll("\"", "").trim().split(".")
-        if(versionName[1] === currentDate) {
-          lines[index] = `versionName = "${versionName[0]}.${versionName[1]}.${parseInt(versionName[2]) + 1}"`
-        } else {
-          lines[index] = `versionName = "${versionName[0]}.${currentDate}.0"`
-        }
-        console.log(`versionName ${versionName} >> ${lines[index]}`)
-        break
-      }
+    // 将文件内容按行分割
+    const lines = content.split("\n");
+    const versionCode = lines.find((line) => line.startsWith("versionCode ="))!.match(/\d+/)![0];
+    const versionName = lines.find((line) => line.startsWith("versionName ="))!.match(/[\d\.]+/)![0];
+
+    let newVersionName = versionName;
+    let newVersionCode = versionCode;
+
+    const versionParts = versionName.split(".");
+    const now = new Date();
+    const currentVersionDate = [now.getFullYear() - 2000, now.getMonth() + 1, now.getDate()]
+      .map((it) => it.toString().padStart(2, "0"))
+      .join("");
+    // 如果日期版本号不一致，那么就进行更新
+    if (currentVersionDate !== versionParts[1]) {
+      newVersionName = [versionParts[0], currentVersionDate, "0"].join(".");
+    }
+    // 如果已经打包过这个文件夹，那么就进行更新
+    else if (fs.existsSync(`${OUTPUT_DIR}/android_v${versionName}`)) {
+      newVersionName = [versionParts[0], currentVersionDate, parseInt(versionParts[2]) + 1].join(".");
     }
 
-    await Deno.writeTextFile(filePath, lines.join("\r\n"))
-    console.log("版本更新完成，开始打包...")
+    if (newVersionName != versionName) {
+      newVersionCode = (parseInt(versionCode) + 1).toString();
+      fs.writeFileSync(
+        filePath,
+        lines
+          .map((line) => {
+            if (line.startsWith("versionCode =")) {
+              return line.replace(/\d+/, newVersionCode);
+            } else if (line.startsWith("versionName =")) {
+              return line.replace(/[\d\.]+/, newVersionName);
+            }
+            return line;
+          })
+          .join("\n")
+      );
+      console.log("versionCode =", newVersionCode);
+      console.log("versionName =", newVersionName);
+      console.log("版本更新，开始打包...");
+    } else {
+      console.log("versionCode =", newVersionCode);
+      console.log("versionName =", newVersionName);
+      console.log("版本保持，开始打包...");
+    }
   } catch (error) {
     console.error("版本更新失败:", error);
   }
-}
+};
+
+// 发布版本的时候，升级下版本信息 versionCode和versionName
+// await upgradeVersionInfo(resolveTo("gradle/libs.versions.toml"));
+// await doBundle();
+await doCopy();
