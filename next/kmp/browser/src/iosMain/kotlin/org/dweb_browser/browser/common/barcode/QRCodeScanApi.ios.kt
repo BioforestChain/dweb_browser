1a1e8fc2c33e88bd7615a4bf21a16fcdb234613a
jackie-yellow <hl19081555@gmail.com>
2024-06-19 18:23:42 +0800

    :ambulance: ios解析二维码

--- next/kmp/browser/src/iosMain/kotlin/org/dweb_browser/browser/common/barcode/QRCodeScanApi.ios.kt
+++ next/kmp/browser/src/iosMain/kotlin/org/dweb_browser/browser/common/barcode/QRCodeScanApi.ios.kt
@@ -6,6 +6,12 @@ import org.dweb_browser.helper.WARNING
 import org.dweb_browser.helper.encodeURIComponent
 import org.dweb_browser.helper.isWebUrl
 import org.dweb_browser.helper.platform.DeepLinkHook
+import org.dweb_browser.helper.platform.toByteArray
+import kotlin.math.abs
+import platform.CoreImage.CIDetector
+import platform.CoreImage.CIImage
+import platform.CoreImage.CIQRCodeFeature
+import platform.Foundation.NSData
 
 actual fun beepAudio() {
   WARNING("Not yet implemented beepAudio")
@@ -16,14 +22,41 @@ actual fun decoderImage(
   onSuccess: (QRCodeDecoderResult) -> Unit,
   onFailure: (Exception) -> Unit,
 ) {
-  WARNING("Not yet implemented decoderImage")
+  val ciImage = CIImage(data = imageBitmap.toByteArray().toNSData())
+  val detector = CIDetector.detectorOfType("CIDetectorTypeQRCode", null, null)
+  detector?.featuresInImage(image = ciImage, options = null)?.let { detectResult ->
+    val listRect: MutableList<QRCodeDecoderResult.QRCode> = mutableListOf()
+    (detectResult as List<CIQRCodeFeature>).map { barcode
+      listRect.add(
+        QRCodeDecoderResult.QRCode(
+          org.dweb_browser.helper.PureRect(
+            x = barcode.boundingBox?.centerX()?.toFloat() ?: 0f,
+            y = barcode.boundingBox?.centerY()?.toFloat() ?: 0f
+          ), barcode.displayValue
+        )
+      )
+      val qrCodeDecoderResult = QRCodeDecoderResult(imageBitmap, imageBitmap, listRect)
+      onSuccess(qrCodeDecoderResult)
+    }
+  }
 }
 
 actual fun transformPoint(
   x: Int, y: Int, srcWidth: Int, srcHeight: Int, destWidth: Int, destHeight: Int, isAlarm: Boolean,
 ): QRCodeDecoderResult.Point {
-  WARNING("Not yet implemented transformPoint")
-  return QRCodeDecoderResult.Point(0f, 0f)
+  val widthRatio = destWidth * 1.0f / srcWidth
+  val heightRatio = destHeight * 1.0f / srcHeight
+  return if (isAlarm) {
+    val ratio = widthRatio.coerceAtMost(heightRatio)
+    val left = abs(srcWidth * ratio - destWidth) / 2
+    val top = abs(srcHeight * ratio - destHeight) / 2
+    QRCodeDecoderResult.Point(x * ratio + left, y * ratio + top)
+  } else {
+    val ratio = widthRatio.coerceAtLeast(heightRatio)
+    val left = abs(srcWidth * ratio - destWidth) / 2
+    val top = abs(srcHeight * ratio - destHeight) / 2
+    QRCodeDecoderResult.Point(x * ratio - left, y * ratio - top)
+  }
 }
 
 actual fun openDeepLink(data: String, showBackground: Boolean): Boolean {
