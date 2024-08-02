d2448b1a15ef409c704a872159f127928363b437
Gaubee <GaubeeBangeel@Gmail.com>
2024-08-02 09:42:05 +0800

    :recycle: [kmp/helperCompose] 修复collectAsMutableState的可靠性，现在更加实时

--- next/kmp/helperCompose/src/commonMain/kotlin/org/dweb_browser/helper/compose/flowExt.kt
+++ next/kmp/helperCompose/src/commonMain/kotlin/org/dweb_browser/helper/compose/flowExt.kt
@@ -1,8 +1,8 @@
 package org.dweb_browser.helper.compose
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.remember
 import kotlinx.coroutines.flow.MutableStateFlow
 
@@ -10,17 +10,16 @@ import kotlinx.coroutines.flow.MutableStateFlow
 @Composable
 fun <T> MutableStateFlow<T>.collectAsMutableState(): MutableState<T> {
   val flow = this
-  val state = flow.collectAsState()
-  return remember(this, state) {
+  val state = remember(flow) {
     object : MutableState<T> {
       override var value: T
-        get() = state.value
+        get() = flow.value
         set(value) {
           flow.value = value
         }
 
       override fun component1(): T {
-        return state.value
+        return flow.value
       }
 
       override fun component2(): (T) -> Unit {
@@ -28,4 +27,10 @@ fun <T> MutableStateFlow<T>.collectAsMutableState(): MutableState<T> {
       }
     }
   }
+  LaunchedEffect(flow, state) {
+    flow.collect {
+      state.value = it
+    }
+  }
+  return state
 }
