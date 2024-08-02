aec4e9e6d52c85ed501a37a1546f359b9372b2c9
Gaubee <GaubeeBangeel@Gmail.com>
2024-08-02 11:35:51 +0800

    :bug: [kmp/heleprCompose] 修复 collectAsMutableState 的实现；从而修复拖动问题

--- next/kmp/helperCompose/src/commonMain/kotlin/org/dweb_browser/helper/compose/flowExt.kt
+++ next/kmp/helperCompose/src/commonMain/kotlin/org/dweb_browser/helper/compose/flowExt.kt
@@ -3,6 +3,7 @@ package org.dweb_browser.helper.compose
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import kotlinx.coroutines.flow.MutableStateFlow
 
@@ -10,27 +11,29 @@ import kotlinx.coroutines.flow.MutableStateFlow
 @Composable
 fun <T> MutableStateFlow<T>.collectAsMutableState(): MutableState<T> {
   val flow = this
-  val state = remember(flow) {
+  val innerState = remember(flow) { mutableStateOf(flow.value) }
+  val outerState = remember(innerState) {
     object : MutableState<T> {
       override var value: T
-        get() = flow.value
+        get() = innerState.value
         set(value) {
+          innerState.value = value
           flow.value = value
         }
 
       override fun component1(): T {
-        return flow.value
+        return value
       }
 
       override fun component2(): (T) -> Unit {
-        return { flow.value = it }
+        return { value = it }
       }
     }
   }
-  LaunchedEffect(flow, state) {
+  LaunchedEffect(flow, innerState) {
     flow.collect {
-      state.value = it
+      innerState.value = it
     }
   }
-  return state
+  return outerState
 }
