6c893e3792ce395a6e4ecc710400a7cf7cccc57e
Kingsword <kingsword09@gmail.com>
2024-09-19 18:08:46 +0800

    :bulb: [kmp/helperPlatform]

--- next/kmp/helperPlatform/src/commonMain/kotlin/org/dweb_browser/helper/platform/ImageBitmap.kt
+++ next/kmp/helperPlatform/src/commonMain/kotlin/org/dweb_browser/helper/platform/ImageBitmap.kt
@@ -1,6 +1,16 @@
 package org.dweb_browser.helper.platform
 
 import androidx.compose.ui.graphics.ImageBitmap
+import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.jetbrains.compose.resources.decodeToImageBitmap
+
+/**
+ * ## 官方 ByteArray 转化为 ImageBitmap
+ * ### 注意: 当前支持的格式 JPEG, PNG, BMP, or WEBP
+ * @see: https://www.jetbrains.com/help/kotlin-multiplatform-dev/whats-new-compose-170.html#functions-for-converting-byte-arrays-into-imagebitmap-or-imagevector
+ * */
+@OptIn(ExperimentalResourceApi::class)
+fun ByteArray.kmpDecodeToImageBitmap() = decodeToImageBitmap()
 
 expect fun ByteArray.toImageBitmap(): ImageBitmap?
 
