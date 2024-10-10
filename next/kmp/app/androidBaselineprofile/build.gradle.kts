c19e4e2e38b351c89a9e1f168cc2e4b83dcc341d
Kingsword <kingsword09@gmail.com>
2024-10-10 18:01:47 +0800

    :bug: [kmp/androidBaselineprofile] 修复 gradle 编译扫描异常

--- next/kmp/app/androidBaselineprofile/build.gradle.kts
+++ next/kmp/app/androidBaselineprofile/build.gradle.kts
@@ -9,12 +9,12 @@ android {
   compileSdk = 34
 
   compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
   }
 
   kotlinOptions {
-    jvmTarget = "1.8"
+    jvmTarget = "17"
   }
 
   defaultConfig {
@@ -22,6 +22,7 @@ android {
     targetSdk = 34
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    missingDimensionStrategy("abi", "forArm64", "release")
   }
 
   targetProjectPath = ":androidApp"
