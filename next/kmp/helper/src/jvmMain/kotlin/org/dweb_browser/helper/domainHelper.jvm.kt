ccc07f0f5c27e5c9467acf1be47fd51c16485cb9
Kingsword <kingsword09@gmail.com>
2025-08-09 12:21:44 +0800

    ⬆️ 🐛 [kmp] 升级kotlin 2.2.0，修复异常

--- next/kmp/helper/src/jvmMain/kotlin/org/dweb_browser/helper/domainHelper.jvm.kt
+++ next/kmp/helper/src/jvmMain/kotlin/org/dweb_browser/helper/domainHelper.jvm.kt
@@ -3,9 +3,9 @@ package org.dweb_browser.helper
 import java.net.IDN
 import java.net.InetAddress
 
-public actual fun String.toPunyCode() = IDN.toASCII(this)!!
+public actual fun String.toPunyCode(): String = IDN.toASCII(this)!!
 
-public actual fun String.isRealDomain() = try {
+public actual fun String.isRealDomain(): Boolean = try {
   InetAddress.getByName(this)
   true
 } catch (e: Throwable) {
